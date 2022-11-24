4801ed43455842f49b9da2f919b17c017e52d7c2
Dewey Dunnington <dewey@fishandwhistle.net>
2022-11-23 22:51:40 -0400

    linestring test

--- src/geoarrow/wkt_writer_test.cc
+++ src/geoarrow/wkt_writer_test.cc
@@ -69,7 +69,8 @@ TEST(WKTWriterTest, WKTWriterTestOneValidOneNull) {
   GeoArrowWKTWriterInitVisitor(&writer, &v);
 
   EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
-  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XY), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XY),
+            GEOARROW_OK);
   EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
   EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
 
@@ -90,69 +91,6 @@ TEST(WKTWriterTest, WKTWriterTestOneValidOneNull) {
   EXPECT_TRUE(ArrowArrayViewIsNull(&view, 1));
   struct ArrowStringView value = ArrowArrayViewGetStringUnsafe(&view, 0);
   EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT EMPTY");
-
-  ArrowArrayViewReset(&view);
-  array.release(&array);
-  GeoArrowWKTWriterReset(&writer);
-}
-
-TEST(WKTWriterTest, WKTWriterTestPoint) {
-  struct GeoArrowWKTWriter writer;
-  struct GeoArrowVisitor v;
-  GeoArrowWKTWriterInit(&writer);
-  GeoArrowWKTWriterInitVisitor(&writer, &v);
-
-  double xs[] = {1};
-  double ys[] = {2};
-  double zs[] = {3};
-  double ms[] = {4};
-  double* coords[] = {xs, ys, zs, ms};
-  double* coords_m[] = {xs, ys, ms};
-
-  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
-  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XY), GEOARROW_OK);
-  EXPECT_EQ(v.coords(&v, (const double**)coords, 1, 2), GEOARROW_OK);
-  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
-  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
-
-  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
-  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XYZ), GEOARROW_OK);
-  EXPECT_EQ(v.coords(&v, (const double**)coords, 1, 3), GEOARROW_OK);
-  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
-  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
-
-  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
-  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XYM), GEOARROW_OK);
-  EXPECT_EQ(v.coords(&v, (const double**)coords_m, 1, 3), GEOARROW_OK);
-  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
-  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
-
-  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
-  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XYZM), GEOARROW_OK);
-  EXPECT_EQ(v.coords(&v, (const double**)coords, 1, 4), GEOARROW_OK);
-  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
-  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
-
-  struct ArrowArray array;
-  EXPECT_EQ(GeoArrowWKTWriterFinish(&writer, &array, nullptr), GEOARROW_OK);
-  EXPECT_EQ(array.length, 4);
-  EXPECT_EQ(array.null_count, 0);
-
-  struct ArrowArrayView view;
-  ArrowArrayViewInit(&view, NANOARROW_TYPE_STRING);
-  ArrowArrayViewSetArray(&view, &array, nullptr);
-
-  struct ArrowStringView value = ArrowArrayViewGetStringUnsafe(&view, 0);
-  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT (1 2)");
-
-  value = ArrowArrayViewGetStringUnsafe(&view, 1);
-  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT Z (1 2 3)");
-
-  value = ArrowArrayViewGetStringUnsafe(&view, 2);
-  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT M (1 2 4)");
-
-  value = ArrowArrayViewGetStringUnsafe(&view, 3);
-  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT ZM (1 2 3 4)");
 
   ArrowArrayViewReset(&view);
   array.release(&array);
@@ -233,4 +171,139 @@ INSTANTIATE_TEST_SUITE_P(WKTWriterTest, GeometryTypeParameterizedTestFixture,
                                            GEOARROW_GEOMETRY_TYPE_MULTIPOLYGON,
                                            GEOARROW_GEOMETRY_TYPE_GEOMETRYCOLLECTION));
 
+TEST(WKTWriterTest, WKTWriterTestPoint) {
+  struct GeoArrowWKTWriter writer;
+  struct GeoArrowVisitor v;
+  GeoArrowWKTWriterInit(&writer);
+  GeoArrowWKTWriterInitVisitor(&writer, &v);
 
+  double xs[] = {1};
+  double ys[] = {2};
+  double zs[] = {3};
+  double ms[] = {4};
+  double* coords[] = {xs, ys, zs, ms};
+  double* coords_m[] = {xs, ys, ms};
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XY),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords, 1, 2), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XYZ),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords, 1, 3), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XYM),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords_m, 1, 3), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_POINT, GEOARROW_DIMENSIONS_XYZM),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords, 1, 4), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  struct ArrowArray array;
+  EXPECT_EQ(GeoArrowWKTWriterFinish(&writer, &array, nullptr), GEOARROW_OK);
+  EXPECT_EQ(array.length, 4);
+  EXPECT_EQ(array.null_count, 0);
+
+  struct ArrowArrayView view;
+  ArrowArrayViewInit(&view, NANOARROW_TYPE_STRING);
+  ArrowArrayViewSetArray(&view, &array, nullptr);
+
+  struct ArrowStringView value = ArrowArrayViewGetStringUnsafe(&view, 0);
+  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT (1 2)");
+
+  value = ArrowArrayViewGetStringUnsafe(&view, 1);
+  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT Z (1 2 3)");
+
+  value = ArrowArrayViewGetStringUnsafe(&view, 2);
+  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT M (1 2 4)");
+
+  value = ArrowArrayViewGetStringUnsafe(&view, 3);
+  EXPECT_EQ(std::string(value.data, value.n_bytes), "POINT ZM (1 2 3 4)");
+
+  ArrowArrayViewReset(&view);
+  array.release(&array);
+  GeoArrowWKTWriterReset(&writer);
+}
+
+TEST(WKTWriterTest, WKTWriterTestLinestring) {
+  struct GeoArrowWKTWriter writer;
+  struct GeoArrowVisitor v;
+  GeoArrowWKTWriterInit(&writer);
+  GeoArrowWKTWriterInitVisitor(&writer, &v);
+
+  double xs[] = {1, 2, 3, 1};
+  double ys[] = {2, 3, 4, 2};
+  double zs[] = {3, 4, 5, 3};
+  double ms[] = {4, 5, 6, 4};
+  double* coords[] = {xs, ys, zs, ms};
+  double* coords_m[] = {xs, ys, ms};
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_LINESTRING, GEOARROW_DIMENSIONS_XY),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords, 4, 2), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_LINESTRING, GEOARROW_DIMENSIONS_XYZ),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords, 4, 3), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_LINESTRING, GEOARROW_DIMENSIONS_XYM),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords_m, 4, 3), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  EXPECT_EQ(v.feat_start(&v), GEOARROW_OK);
+  EXPECT_EQ(v.geom_start(&v, GEOARROW_GEOMETRY_TYPE_LINESTRING, GEOARROW_DIMENSIONS_XYZM),
+            GEOARROW_OK);
+  EXPECT_EQ(v.coords(&v, (const double**)coords, 4, 4), GEOARROW_OK);
+  EXPECT_EQ(v.geom_end(&v), GEOARROW_OK);
+  EXPECT_EQ(v.feat_end(&v), GEOARROW_OK);
+
+  struct ArrowArray array;
+  EXPECT_EQ(GeoArrowWKTWriterFinish(&writer, &array, nullptr), GEOARROW_OK);
+  EXPECT_EQ(array.length, 4);
+  EXPECT_EQ(array.null_count, 0);
+
+  struct ArrowArrayView view;
+  ArrowArrayViewInit(&view, NANOARROW_TYPE_STRING);
+  ArrowArrayViewSetArray(&view, &array, nullptr);
+
+  struct ArrowStringView value = ArrowArrayViewGetStringUnsafe(&view, 0);
+  EXPECT_EQ(std::string(value.data, value.n_bytes), "LINESTRING (1 2, 2 3, 3 4, 1 2)");
+
+  value = ArrowArrayViewGetStringUnsafe(&view, 1);
+  EXPECT_EQ(std::string(value.data, value.n_bytes),
+            "LINESTRING Z (1 2 3, 2 3 4, 3 4 5, 1 2 3)");
+
+  value = ArrowArrayViewGetStringUnsafe(&view, 2);
+  EXPECT_EQ(std::string(value.data, value.n_bytes),
+            "LINESTRING M (1 2 4, 2 3 5, 3 4 6, 1 2 4)");
+
+  value = ArrowArrayViewGetStringUnsafe(&view, 3);
+  EXPECT_EQ(std::string(value.data, value.n_bytes),
+            "LINESTRING ZM (1 2 3 4, 2 3 4 5, 3 4 5 6, 1 2 3 4)");
+
+  ArrowArrayViewReset(&view);
+  array.release(&array);
+  GeoArrowWKTWriterReset(&writer);
+}
