6c74d5402b7a4f7520564bdcf5a26a12b3ae0de1
Dewey Dunnington <dewey@fishandwhistle.net>
2022-12-12 15:31:02 -0400

    test all types for empty/init

--- src/geoarrow/array_test.cc
+++ src/geoarrow/array_test.cc
@@ -4,30 +4,62 @@
 #include "geoarrow.h"
 #include "nanoarrow.h"
 
-TEST(ArrayTest, ArrayTestInit) {
+class TypeParameterizedTestFixture : public ::testing::TestWithParam<enum GeoArrowType> {
+ protected:
+  enum GeoArrowType type;
+};
+
+TEST_P(TypeParameterizedTestFixture, ArrayTestInit) {
   struct GeoArrowArray array;
   struct ArrowSchema schema;
+  enum GeoArrowType type = GetParam();
 
-  EXPECT_EQ(GeoArrowArrayInitFromType(&array, GEOARROW_TYPE_POINT), GEOARROW_OK);
-  EXPECT_EQ(array.schema_view.type, GEOARROW_TYPE_POINT);
+  EXPECT_EQ(GeoArrowArrayInitFromType(&array, type), GEOARROW_OK);
+  EXPECT_EQ(array.schema_view.type, type);
   GeoArrowArrayReset(&array);
 
-  ASSERT_EQ(GeoArrowSchemaInitExtension(&schema, GEOARROW_TYPE_POINT), GEOARROW_OK);
+  ASSERT_EQ(GeoArrowSchemaInitExtension(&schema, type), GEOARROW_OK);
   EXPECT_EQ(GeoArrowArrayInitFromSchema(&array, &schema, nullptr), GEOARROW_OK);
   GeoArrowArrayReset(&array);
   schema.release(&schema);
 }
 
-TEST(ArrayTest, ArrayTestEmpty) {
+TEST_P(TypeParameterizedTestFixture, ArrayTestEmpty) {
   struct GeoArrowArray array;
   struct ArrowArray array_out;
   array_out.release = nullptr;
+  enum GeoArrowType type = GetParam();
 
-  EXPECT_EQ(GeoArrowArrayInitFromType(&array, GEOARROW_TYPE_POINT), GEOARROW_OK);
+  EXPECT_EQ(GeoArrowArrayInitFromType(&array, type), GEOARROW_OK);
   EXPECT_EQ(GeoArrowArrayFinish(&array, &array_out, nullptr), GEOARROW_OK);
   EXPECT_NE(array_out.release, nullptr);
   EXPECT_EQ(array.array.release, nullptr);
-
   GeoArrowArrayReset(&array);
+
+  // Make sure this is a valid zero-length array
+  struct ArrowArrayView array_view;
+  struct ArrowSchema schema;
+  ASSERT_EQ(GeoArrowSchemaInit(&schema, type), GEOARROW_OK);
+  EXPECT_EQ(ArrowArrayViewInitFromSchema(&array_view, &schema, nullptr), GEOARROW_OK);
+
+  ArrowArrayViewReset(&array_view);
   array_out.release(&array_out);
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    ArrayTest, TypeParameterizedTestFixture,
+    ::testing::Values(GEOARROW_TYPE_POINT, GEOARROW_TYPE_LINESTRING,
+                      GEOARROW_TYPE_POLYGON, GEOARROW_TYPE_MULTIPOINT,
+                      GEOARROW_TYPE_MULTILINESTRING, GEOARROW_TYPE_MULTIPOLYGON,
+
+                      GEOARROW_TYPE_POINT_Z, GEOARROW_TYPE_LINESTRING_Z,
+                      GEOARROW_TYPE_POLYGON_Z, GEOARROW_TYPE_MULTIPOINT_Z,
+                      GEOARROW_TYPE_MULTILINESTRING_Z, GEOARROW_TYPE_MULTIPOLYGON_Z,
+
+                      GEOARROW_TYPE_POINT_M, GEOARROW_TYPE_LINESTRING_M,
+                      GEOARROW_TYPE_POLYGON_M, GEOARROW_TYPE_MULTIPOINT_M,
+                      GEOARROW_TYPE_MULTILINESTRING_M, GEOARROW_TYPE_MULTIPOLYGON_M,
+
+                      GEOARROW_TYPE_POINT_ZM, GEOARROW_TYPE_LINESTRING_ZM,
+                      GEOARROW_TYPE_POLYGON_ZM, GEOARROW_TYPE_MULTIPOINT_ZM,
+                      GEOARROW_TYPE_MULTILINESTRING_ZM, GEOARROW_TYPE_MULTIPOLYGON_ZM));
