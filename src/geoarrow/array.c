72a455a18240f3360c37aed1ea79e9d2ba62c859
Dewey Dunnington <dewey@fishandwhistle.net>
2022-12-12 22:29:10 -0400

    implement length setting

--- src/geoarrow/array.c
+++ src/geoarrow/array.c
@@ -24,16 +24,6 @@ GeoArrowErrorCode GeoArrowArrayInitFromType(struct GeoArrowArray* array,
   return result;
 }
 
-GeoArrowErrorCode GeoArrowArrayFinish(struct GeoArrowArray* array,
-                                      struct ArrowArray* array_out,
-                                      struct GeoArrowError* error) {
-  NANOARROW_RETURN_NOT_OK(
-      ArrowArrayFinishBuilding(&array->array, (struct ArrowError*)error));
-  memcpy(array_out, &array->array, sizeof(struct ArrowArray));
-  array->array.release = NULL;
-  return GEOARROW_OK;
-}
-
 void GeoArrowArrayReset(struct GeoArrowArray* array) {
   if (array->array.release != NULL) {
     array->array.release(&array->array);
@@ -73,6 +63,54 @@ static int GeoArrowArrayFindBuffer(struct ArrowArray* array,
   return total_buffers;
 }
 
+static void GeoArrowSetArrayLengthFromBufferLength(struct GeoArrowSchemaView* schema_view,
+                                                   struct GeoArrowFindBufferResult* res,
+                                                   int64_t size_bytes) {
+  // By luck, buffer index 1 for every array is the one we use to infer the length;
+  // however, this is a slightly different formula for each type/depth
+  if (res->i != 1) {
+    return;
+  }
+
+  switch (schema_view->type) {
+    case GEOARROW_TYPE_WKB:
+      res->array->length = (size_bytes / sizeof(int32_t)) - 1;
+      return;
+    case GEOARROW_TYPE_LARGE_WKB:
+      res->array->length = (size_bytes / sizeof(int64_t)) - 1;
+      return;
+    default:
+      break;
+  }
+
+  int coord_level;
+  switch (schema_view->geometry_type) {
+    case GEOARROW_GEOMETRY_TYPE_POINT:
+      coord_level = 0;
+      break;
+    case GEOARROW_GEOMETRY_TYPE_LINESTRING:
+    case GEOARROW_GEOMETRY_TYPE_MULTIPOINT:
+      coord_level = 1;
+      break;
+    case GEOARROW_GEOMETRY_TYPE_POLYGON:
+    case GEOARROW_GEOMETRY_TYPE_MULTILINESTRING:
+      coord_level = 2;
+      break;
+    case GEOARROW_GEOMETRY_TYPE_MULTIPOLYGON:
+      coord_level = 3;
+    default:
+      return;
+  }
+
+  if (res->level < coord_level) {
+    // This is an offset buffer
+    res->array->length = (size_bytes / sizeof(int32_t)) - 1;
+  } else {
+    // This is a data buffer
+    res->array->length = size_bytes / sizeof(double);
+  }
+}
+
 GeoArrowErrorCode GeoArrowArraySetBufferCopy(struct GeoArrowArray* array, int64_t i,
                                              struct GeoArrowBufferView value) {
   struct GeoArrowFindBufferResult res;
@@ -89,5 +127,68 @@ GeoArrowErrorCode GeoArrowArraySetBufferCopy(struct GeoArrowArray* array, int64_
   struct ArrowBuffer* buffer = ArrowArrayBuffer(res.array, res.i);
   NANOARROW_RETURN_NOT_OK(ArrowBufferAppend(buffer, value.data, value.n_bytes));
 
+  // Possibly set the array length from the buffer length
+  GeoArrowSetArrayLengthFromBufferLength(&array->schema_view, &res, value.n_bytes);
+
+  return GEOARROW_OK;
+}
+
+GeoArrowErrorCode GeoArrowArrayFinish(struct GeoArrowArray* array,
+                                      struct ArrowArray* array_out,
+                                      struct GeoArrowError* error) {
+  // At this point all the array lengths should be set except for the
+  // fixed-size list or struct parent to the coordinate array(s).
+  int scale;
+  switch (array->schema_view.coord_type) {
+    case GEOARROW_COORD_TYPE_SEPARATE:
+      scale = 1;
+      break;
+    case GEOARROW_COORD_TYPE_INTERLEAVED:
+      switch (array->schema_view.dimensions) {
+        case GEOARROW_DIMENSIONS_XY:
+          scale = 2;
+          break;
+        case GEOARROW_DIMENSIONS_XYZ:
+        case GEOARROW_DIMENSIONS_XYM:
+          scale = 3;
+          break;
+        case GEOARROW_DIMENSIONS_XYZM:
+          scale = 4;
+          break;
+        default:
+          return EINVAL;
+      }
+      break;
+    default:
+      // e.g., WKB
+      break;
+  }
+
+  switch (array->schema_view.geometry_type) {
+    case GEOARROW_GEOMETRY_TYPE_POINT:
+      array->array.length = array->array.children[0]->length;
+      break;
+    case GEOARROW_GEOMETRY_TYPE_LINESTRING:
+    case GEOARROW_GEOMETRY_TYPE_MULTIPOINT:
+      array->array.children[0]->length = array->array.children[0]->children[0]->length;
+      break;
+    case GEOARROW_GEOMETRY_TYPE_POLYGON:
+    case GEOARROW_GEOMETRY_TYPE_MULTILINESTRING:
+      array->array.children[0]->length =
+          array->array.children[0]->children[0]->children[0]->length;
+      break;
+    case GEOARROW_GEOMETRY_TYPE_MULTIPOLYGON:
+      array->array.children[0]->length =
+          array->array.children[0]->children[0]->children[0]->children[0]->length;
+      break;
+    default:
+      // e.g., WKB
+      break;
+  }
+
+  NANOARROW_RETURN_NOT_OK(
+      ArrowArrayFinishBuilding(&array->array, (struct ArrowError*)error));
+  memcpy(array_out, &array->array, sizeof(struct ArrowArray));
+  array->array.release = NULL;
   return GEOARROW_OK;
 }
