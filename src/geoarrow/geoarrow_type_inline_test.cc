29fa090b182259194580c2daf8af93a2fb7b94f2
Dewey Dunnington <dewey@fishandwhistle.net>
2023-01-28 14:18:47 -0400

    add xy-to- copy tests

--- src/geoarrow/geoarrow_type_inline_test.cc
+++ src/geoarrow/geoarrow_type_inline_test.cc
@@ -1,4 +1,6 @@
 
+#include <cmath>
+
 #include <gtest/gtest.h>
 
 #include "geoarrow.h"
@@ -127,4 +129,88 @@ TEST(TypeInlineTest, TypeInlineTestCopyCoordsXYtoXY) {
   EXPECT_EQ(dst.storage()[1][2], 7);
   EXPECT_EQ(dst.storage()[1][3], 8);
   EXPECT_EQ(dst.storage()[1][4], 9);
+}
+
+TEST(TypeInlineTest, TypeInlineTestCopyCoordsXYtoXYZ) {
+  TestCoords src({1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15},
+                 {16, 17, 18, 19, 20});
+  TestCoords dst({0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0});
+
+  GeoArrowCoordViewCopy(src.view(), GEOARROW_DIMENSIONS_XY, 1, dst.writable_view(),
+                        GEOARROW_DIMENSIONS_XYZ, 2, 3);
+  EXPECT_EQ(dst.storage()[0][0], 0);
+  EXPECT_EQ(dst.storage()[0][1], 0);
+  EXPECT_EQ(dst.storage()[0][2], 2);
+  EXPECT_EQ(dst.storage()[0][3], 3);
+  EXPECT_EQ(dst.storage()[0][4], 4);
+
+  EXPECT_EQ(dst.storage()[1][0], 0);
+  EXPECT_EQ(dst.storage()[1][1], 0);
+  EXPECT_EQ(dst.storage()[1][2], 7);
+  EXPECT_EQ(dst.storage()[1][3], 8);
+  EXPECT_EQ(dst.storage()[1][4], 9);
+
+  EXPECT_EQ(dst.storage()[2][0], 0);
+  EXPECT_EQ(dst.storage()[2][1], 0);
+  EXPECT_TRUE(isnan(dst.storage()[2][2]));
+  EXPECT_TRUE(isnan(dst.storage()[2][3]));
+  EXPECT_TRUE(isnan(dst.storage()[2][4]));
+}
+
+TEST(TypeInlineTest, TypeInlineTestCopyCoordsXYtoXYM) {
+  TestCoords src({1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15},
+                 {16, 17, 18, 19, 20});
+  TestCoords dst({0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0});
+
+  GeoArrowCoordViewCopy(src.view(), GEOARROW_DIMENSIONS_XY, 1, dst.writable_view(),
+                        GEOARROW_DIMENSIONS_XYM, 2, 3);
+  EXPECT_EQ(dst.storage()[0][0], 0);
+  EXPECT_EQ(dst.storage()[0][1], 0);
+  EXPECT_EQ(dst.storage()[0][2], 2);
+  EXPECT_EQ(dst.storage()[0][3], 3);
+  EXPECT_EQ(dst.storage()[0][4], 4);
+
+  EXPECT_EQ(dst.storage()[1][0], 0);
+  EXPECT_EQ(dst.storage()[1][1], 0);
+  EXPECT_EQ(dst.storage()[1][2], 7);
+  EXPECT_EQ(dst.storage()[1][3], 8);
+  EXPECT_EQ(dst.storage()[1][4], 9);
+
+  EXPECT_EQ(dst.storage()[2][0], 0);
+  EXPECT_EQ(dst.storage()[2][1], 0);
+  EXPECT_TRUE(isnan(dst.storage()[2][2]));
+  EXPECT_TRUE(isnan(dst.storage()[2][3]));
+  EXPECT_TRUE(isnan(dst.storage()[2][4]));
+}
+
+TEST(TypeInlineTest, TypeInlineTestCopyCoordsXYtoXYZM) {
+  TestCoords src({1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15},
+                 {16, 17, 18, 19, 20});
+  TestCoords dst({0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0});
+
+  GeoArrowCoordViewCopy(src.view(), GEOARROW_DIMENSIONS_XY, 1, dst.writable_view(),
+                        GEOARROW_DIMENSIONS_XYZM, 2, 3);
+  EXPECT_EQ(dst.storage()[0][0], 0);
+  EXPECT_EQ(dst.storage()[0][1], 0);
+  EXPECT_EQ(dst.storage()[0][2], 2);
+  EXPECT_EQ(dst.storage()[0][3], 3);
+  EXPECT_EQ(dst.storage()[0][4], 4);
+
+  EXPECT_EQ(dst.storage()[1][0], 0);
+  EXPECT_EQ(dst.storage()[1][1], 0);
+  EXPECT_EQ(dst.storage()[1][2], 7);
+  EXPECT_EQ(dst.storage()[1][3], 8);
+  EXPECT_EQ(dst.storage()[1][4], 9);
+
+  EXPECT_EQ(dst.storage()[2][0], 0);
+  EXPECT_EQ(dst.storage()[2][1], 0);
+  EXPECT_TRUE(isnan(dst.storage()[2][2]));
+  EXPECT_TRUE(isnan(dst.storage()[2][3]));
+  EXPECT_TRUE(isnan(dst.storage()[2][4]));
+
+  EXPECT_EQ(dst.storage()[3][0], 0);
+  EXPECT_EQ(dst.storage()[3][1], 0);
+  EXPECT_TRUE(isnan(dst.storage()[3][2]));
+  EXPECT_TRUE(isnan(dst.storage()[3][3]));
+  EXPECT_TRUE(isnan(dst.storage()[3][4]));
 }
