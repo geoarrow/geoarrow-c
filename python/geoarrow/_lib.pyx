3bc9bfdf5150f42b532295eccb4c99dd18c47c0e
Dewey Dunnington <dewey@fishandwhistle.net>
2023-03-12 22:15:17 -0300

    add a few type declarations

--- python/geoarrow/_lib.pyx
+++ python/geoarrow/_lib.pyx
@@ -1,7 +1,94 @@
 
 # cython: language_level = 3
 
-"""Low-level nanoarrow Python bindings."""
+"""Low-level geoarrow Python bindings."""
+
+from libc.stdint cimport int64_t, uintptr_t
+
+cdef extern from "geoarrow_type.h":
+    struct ArrowSchema:
+        const char* format
+        const char* name
+        const char* metadata
+        int64_t flags
+        int64_t n_children
+        ArrowSchema** children
+        ArrowSchema* dictionary
+        void (*release)(ArrowSchema*)
+        void* private_data
+
+    struct ArrowArray:
+        int64_t length
+        int64_t null_count
+        int64_t offset
+        int64_t n_buffers
+        int64_t n_children
+        const void** buffers
+        ArrowArray** children
+        ArrowArray* dictionary
+        void (*release)(ArrowArray*)
+        void* private_data
+
+    cpdef enum GeoArrowGeometryType:
+        GEOARROW_GEOMETRY_TYPE_GEOMETRY = 0
+        GEOARROW_GEOMETRY_TYPE_POINT = 1
+        GEOARROW_GEOMETRY_TYPE_LINESTRING = 2
+        GEOARROW_GEOMETRY_TYPE_POLYGON = 3
+        GEOARROW_GEOMETRY_TYPE_MULTIPOINT = 4
+        GEOARROW_GEOMETRY_TYPE_MULTILINESTRING = 5
+        GEOARROW_GEOMETRY_TYPE_MULTIPOLYGON = 6
+        GEOARROW_GEOMETRY_TYPE_GEOMETRYCOLLECTION = 7
+
+    cpdef enum GeoArrowDimensions:
+        GEOARROW_DIMENSIONS_UNKNOWN = 0
+        GEOARROW_DIMENSIONS_XY = 1
+        GEOARROW_DIMENSIONS_XYZ = 2
+        GEOARROW_DIMENSIONS_XYM = 3
+        GEOARROW_DIMENSIONS_XYZM = 4
+
+    cpdef enum GeoArrowCoordType:
+        GEOARROW_COORD_TYPE_UNKNOWN = 0
+        GEOARROW_COORD_TYPE_SEPARATE = 1
+        GEOARROW_COORD_TYPE_INTERLEAVED = 2
+
+    cpdef enum GeoArrowType:
+        pass
+
+    cpdef enum GeoArrowEdgeType:
+        GEOARROW_EDGE_TYPE_PLANAR
+        GEOARROW_EDGE_TYPE_SPHERICAL
+
+    cpdef enum GeoArrowCrsType:
+        GEOARROW_CRS_TYPE_NONE
+        GEOARROW_CRS_TYPE_UNKNOWN
+        GEOARROW_CRS_TYPE_PROJJSON
+
+
+cdef class SchemaHolder:
+    cdef ArrowSchema c_schema
+
+    def __init__(self):
+        self.c_schema.release = NULL
+
+    def __del__(self):
+        if self.c_schema.release != NULL:
+          self.c_schema.release(&self.c_schema)
+
+    def _addr(self):
+        return <uintptr_t>&self.c_schema
+
+cdef class ArrayHolder:
+    cdef ArrowArray c_array
+
+    def __init__(self):
+        self.c_array.release = NULL
+
+    def __del__(self):
+        if self.c_array.release != NULL:
+          self.c_array.release(&self.c_array)
+
+    def _addr(self):
+        return <uintptr_t>&self.c_array
 
 def some_function():
     return True
