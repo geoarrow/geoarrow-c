4e8828f759bfa5bc96f110f4b1f09e52fe6c3021
Dewey Dunnington <dewey@dunnington.ca>
2023-05-04 21:59:59 -0400

    reformat with black (#22)

--- python/tests/test_geoarrow_pyarrow.py
+++ python/tests/test_geoarrow_pyarrow.py
@@ -1,4 +1,3 @@
-
 import sys
 
 import pyarrow as pa
@@ -8,11 +7,10 @@ import pytest
 import geoarrow.lib as lib
 import geoarrow.pyarrow as ga
 
+
 def test_vector_type_basic():
     ctype = lib.CVectorType.Make(
-        ga.GeometryType.POINT,
-        ga.Dimensions.XY,
-        ga.CoordType.SEPARATE
+        ga.GeometryType.POINT, ga.Dimensions.XY, ga.CoordType.SEPARATE
     )
 
     pa_type = ga.PointType(ctype)
@@ -22,18 +20,17 @@ def test_vector_type_basic():
     assert pa_type.coord_type == ga.CoordType.SEPARATE
 
     expected_storage = pa.struct(
-        [pa.field('x', pa.float64()), pa.field('y', pa.float64())]
+        [pa.field("x", pa.float64()), pa.field("y", pa.float64())]
     )
     assert pa_type.storage_type == expected_storage
 
     with pytest.raises(ValueError):
         ga.LinestringType(ctype)
 
+
 def test_vector_type_with():
     ctype = lib.CVectorType.Make(
-        ga.GeometryType.POINT,
-        ga.Dimensions.XY,
-        ga.CoordType.SEPARATE
+        ga.GeometryType.POINT, ga.Dimensions.XY, ga.CoordType.SEPARATE
     )
 
     type_obj = ga.PointType(ctype)
@@ -50,35 +47,37 @@ def test_vector_type_with():
     type_spherical = type_obj.with_edge_type(ga.EdgeType.SPHERICAL)
     assert type_spherical.edge_type == ga.EdgeType.SPHERICAL
 
-    type_crs = type_obj.with_crs('EPSG:1234', ga.CrsType.UNKNOWN)
+    type_crs = type_obj.with_crs("EPSG:1234", ga.CrsType.UNKNOWN)
     assert type_crs.crs_type == ga.CrsType.UNKNOWN
-    assert type_crs.crs == 'EPSG:1234'
+    assert type_crs.crs == "EPSG:1234"
+
 
 def test_constructors():
-    assert ga.wkb().extension_name == 'geoarrow.wkb'
-    assert ga.large_wkb().extension_name == 'geoarrow.wkb'
-    assert ga.wkt().extension_name == 'geoarrow.wkt'
-    assert ga.large_wkt().extension_name == 'geoarrow.wkt'
-    assert ga.point().extension_name == 'geoarrow.point'
-    assert ga.linestring().extension_name == 'geoarrow.linestring'
-    assert ga.polygon().extension_name == 'geoarrow.polygon'
-    assert ga.multipoint().extension_name == 'geoarrow.multipoint'
-    assert ga.multilinestring().extension_name == 'geoarrow.multilinestring'
-    assert ga.multipolygon().extension_name == 'geoarrow.multipolygon'
+    assert ga.wkb().extension_name == "geoarrow.wkb"
+    assert ga.large_wkb().extension_name == "geoarrow.wkb"
+    assert ga.wkt().extension_name == "geoarrow.wkt"
+    assert ga.large_wkt().extension_name == "geoarrow.wkt"
+    assert ga.point().extension_name == "geoarrow.point"
+    assert ga.linestring().extension_name == "geoarrow.linestring"
+    assert ga.polygon().extension_name == "geoarrow.polygon"
+    assert ga.multipoint().extension_name == "geoarrow.multipoint"
+    assert ga.multilinestring().extension_name == "geoarrow.multilinestring"
+    assert ga.multipolygon().extension_name == "geoarrow.multipolygon"
 
     generic = ga.vector_type(
         ga.GeometryType.POINT,
         ga.Dimensions.XYZ,
         ga.CoordType.INTERLEAVED,
         ga.EdgeType.SPHERICAL,
-        'EPSG:1234'
+        "EPSG:1234",
     )
     assert generic.geometry_type == ga.GeometryType.POINT
     assert generic.dimensions == ga.Dimensions.XYZ
     assert generic.coord_type == ga.CoordType.INTERLEAVED
     assert generic.edge_type == ga.EdgeType.SPHERICAL
-    assert generic.crs == 'EPSG:1234'
+    assert generic.crs == "EPSG:1234"
     assert generic.crs_type == ga.CrsType.UNKNOWN
+
 
 def test_register_extension_types():
     # Unregistering once is ok
@@ -107,7 +106,7 @@ def test_array():
     array = ga.array(["POINT (30 10)"])
     assert array.type == ga.wkt()
 
-    wkb_item = b'\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3e\x40\x00\x00\x00\x00\x00\x00\x24\x40'
+    wkb_item = b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3e\x40\x00\x00\x00\x00\x00\x00\x24\x40"
     array = ga.array([wkb_item])
     assert array.type == ga.wkb()
 
@@ -132,8 +131,9 @@ def test_array():
     assert array.type == ga.large_wkb()
     assert array.type.storage_type == pa.large_binary()
 
+
 def test_array_as():
-    array = ga.array(['POINT (30 10)'])
+    array = ga.array(["POINT (30 10)"])
     array_wkb = array.as_wkb()
 
     array_as_wkt = array.as_wkt()
@@ -165,92 +165,97 @@ def test_array_as():
     with pytest.raises(NotImplementedError):
         array.as_geoarrow(None)
 
+
 def test_array_repr():
-    array = ga.array(['POINT (30 10)'])
+    array = ga.array(["POINT (30 10)"])
     array_repr = repr(array)
-    assert array_repr.startswith('VectorArray')
-    assert '<POINT (30 10)>' in array_repr
+    assert array_repr.startswith("VectorArray")
+    assert "<POINT (30 10)>" in array_repr
 
-    array = ga.array(['POINT (30 10)'] * 12)
+    array = ga.array(["POINT (30 10)"] * 12)
     array_repr = repr(array)
-    assert '...2 values...' in array_repr
+    assert "...2 values..." in array_repr
 
-    array = ga.array([
-        'LINESTRING (100000 100000, 100000 100000, 100000 100000, 100000 100000)'
-    ])
+    array = ga.array(
+        ["LINESTRING (100000 100000, 100000 100000, 100000 100000, 100000 100000)"]
+    )
     array_repr = repr(array)
-    assert '...>' in array_repr
+    assert "...>" in array_repr
 
-    array = ga.array(['THIS IS TOTALLY INVALID WKT'], validate=False)
+    array = ga.array(["THIS IS TOTALLY INVALID WKT"], validate=False)
     array_repr = repr(array)
-    assert array_repr.startswith('VectorArray')
-    assert '* 1 or more display values failed to parse' in array_repr
+    assert array_repr.startswith("VectorArray")
+    assert "* 1 or more display values failed to parse" in array_repr
+
 
 def test_kernel_void():
     with pytest.raises(TypeError):
         kernel = ga.Kernel.void(pa.int32())
         kernel.push(5)
 
-    array = ga.array(['POINT (30 10)'])
+    array = ga.array(["POINT (30 10)"])
     kernel = ga.Kernel.void(array.type)
     out = kernel.push(array)
     assert out.type == pa.null()
     assert len(out) == 1
 
-    array = ga.array(['POINT (30 10)', 'POINT (31 11)'])
+    array = ga.array(["POINT (30 10)", "POINT (31 11)"])
     kernel = ga.Kernel.void_agg(array.type)
     assert kernel.push(array) is None
     out = kernel.finish()
     assert out.type == pa.null()
     assert len(out) == 1
 
+
 def test_kernel_as():
-    array = ga.array(['POINT (30 10)'], ga.wkt().with_crs('EPSG:1234'))
+    array = ga.array(["POINT (30 10)"], ga.wkt().with_crs("EPSG:1234"))
     kernel = ga.Kernel.as_wkt(array.type)
     out = kernel.push(array)
-    assert out.type.extension_name == 'geoarrow.wkt'
-    assert out.type.crs == 'EPSG:1234'
+    assert out.type.extension_name == "geoarrow.wkt"
+    assert out.type.crs == "EPSG:1234"
     assert isinstance(out, ga.VectorArray)
 
-    array = ga.array(['POINT (30 10)'], ga.wkt().with_crs('EPSG:1234'))
+    array = ga.array(["POINT (30 10)"], ga.wkt().with_crs("EPSG:1234"))
     kernel = ga.Kernel.as_wkb(array.type)
     out = kernel.push(array)
-    assert out.type.extension_name == 'geoarrow.wkb'
-    assert out.type.crs == 'EPSG:1234'
+    assert out.type.extension_name == "geoarrow.wkb"
+    assert out.type.crs == "EPSG:1234"
     assert isinstance(out, ga.VectorArray)
 
-    if sys.byteorder == 'little':
-        wkb_item = b'\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3e\x40\x00\x00\x00\x00\x00\x00\x24\x40'
+    if sys.byteorder == "little":
+        wkb_item = b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x3e\x40\x00\x00\x00\x00\x00\x00\x24\x40"
         assert out[0].as_py() == wkb_item
 
-    array = ga.array(['POINT (30 10)'], ga.wkt().with_crs('EPSG:1234'))
+    array = ga.array(["POINT (30 10)"], ga.wkt().with_crs("EPSG:1234"))
     kernel = ga.Kernel.as_geoarrow(array.type, ga.point().id)
     out = kernel.push(array)
-    assert out.type.extension_name == 'geoarrow.point'
-    assert out.type.crs == 'EPSG:1234'
+    assert out.type.extension_name == "geoarrow.point"
+    assert out.type.crs == "EPSG:1234"
     assert isinstance(out, ga.PointArray)
 
+
 def test_kernel_format():
-    array = ga.array(['POINT (30.12345 10.12345)'])
+    array = ga.array(["POINT (30.12345 10.12345)"])
     kernel = ga.Kernel.format_wkt(
-        array.type,
-        significant_digits=5,
-        max_element_size_bytes=15
+        array.type, significant_digits=5, max_element_size_bytes=15
     )
 
     out = kernel.push(array)
     assert out.type == pa.string()
-    assert out[0].as_py() == 'POINT (30.123 1'
+    assert out[0].as_py() == "POINT (30.123 1"
+
 
 def test_kernel_visit_void():
-    array = ga.array(['POINT (30 10)'], ga.wkt())
+    array = ga.array(["POINT (30 10)"], ga.wkt())
     kernel = ga.Kernel.visit_void_agg(array.type)
     assert kernel.push(array) is None
     out = kernel.finish()
     assert out.type == pa.null()
     assert len(out) == 1
 
-    array = ga.array(['POINT (30 10)', 'NOT VALID WKT AT ALL'], ga.wkt(), validate=False)
+    array = ga.array(
+        ["POINT (30 10)", "NOT VALID WKT AT ALL"], ga.wkt(), validate=False
+    )
     kernel = ga.Kernel.visit_void_agg(array.type)
     with pytest.raises(ValueError):
         kernel.push(array) is None
@@ -260,7 +265,7 @@ def test_kernel_visit_void():
 
 
 def test_array_geobuffers():
-    arr = ga.array(['POLYGON ((0 0, 1 0, 0 1, 0 0))']).as_geoarrow(ga.polygon())
+    arr = ga.array(["POLYGON ((0 0, 1 0, 0 1, 0 0))"]).as_geoarrow(ga.polygon())
     bufs = arr.geobuffers()
     assert bufs[0] is None
     np.testing.assert_array_equal(bufs[1], np.array([0, 1]))
@@ -268,21 +273,24 @@ def test_array_geobuffers():
     np.testing.assert_array_equal(bufs[3], np.array([0.0, 1.0, 0.0, 0.0]))
     np.testing.assert_array_equal(bufs[4], np.array([0.0, 0.0, 1.0, 0.0]))
 
+
 def test_point_array_from_geobuffers():
     arr = ga.point().from_geobuffers(
-        b'\xff',
+        b"\xff",
         np.array([1.0, 2.0, 3.0]),
         np.array([4.0, 5.0, 6.0]),
     )
     assert len(arr) == 3
-    assert arr.as_wkt()[2].as_py() == 'POINT (3 6)'
+    assert arr.as_wkt()[2].as_py() == "POINT (3 6)"
 
-    arr = ga.point().with_coord_type(ga.CoordType.INTERLEAVED).from_geobuffers(
-        None,
-        np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
+    arr = (
+        ga.point()
+        .with_coord_type(ga.CoordType.INTERLEAVED)
+        .from_geobuffers(None, np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]))
     )
     assert len(arr) == 3
-    assert arr.as_wkt()[2].as_py() == 'POINT (5 6)'
+    assert arr.as_wkt()[2].as_py() == "POINT (5 6)"
+
 
 def test_linestring_array_from_geobuffers():
     arr = ga.linestring().from_geobuffers(
@@ -292,7 +300,8 @@ def test_linestring_array_from_geobuffers():
         np.array([4.0, 5.0, 6.0]),
     )
     assert len(arr) == 1
-    assert arr.as_wkt()[0].as_py() == 'LINESTRING (1 4, 2 5, 3 6)'
+    assert arr.as_wkt()[0].as_py() == "LINESTRING (1 4, 2 5, 3 6)"
+
 
 def test_polygon_array_from_geobuffers():
     arr = ga.polygon().from_geobuffers(
@@ -303,7 +312,8 @@ def test_polygon_array_from_geobuffers():
         np.array([4.0, 5.0, 6.0, 4.0]),
     )
     assert len(arr) == 1
-    assert arr.as_wkt()[0].as_py() == 'POLYGON ((1 4, 2 5, 3 6, 1 4))'
+    assert arr.as_wkt()[0].as_py() == "POLYGON ((1 4, 2 5, 3 6, 1 4))"
+
 
 def test_multipoint_array_from_geobuffers():
     arr = ga.multipoint().from_geobuffers(
@@ -313,7 +323,8 @@ def test_multipoint_array_from_geobuffers():
         np.array([4.0, 5.0, 6.0]),
     )
     assert len(arr) == 1
-    assert arr.as_wkt()[0].as_py() == 'MULTIPOINT (1 4, 2 5, 3 6)'
+    assert arr.as_wkt()[0].as_py() == "MULTIPOINT (1 4, 2 5, 3 6)"
+
 
 def test_multilinestring_array_from_geobuffers():
     arr = ga.multilinestring().from_geobuffers(
@@ -324,7 +335,8 @@ def test_multilinestring_array_from_geobuffers():
         np.array([4.0, 5.0, 6.0, 4.0]),
     )
     assert len(arr) == 1
-    assert arr.as_wkt()[0].as_py() == 'MULTILINESTRING ((1 4, 2 5, 3 6, 1 4))'
+    assert arr.as_wkt()[0].as_py() == "MULTILINESTRING ((1 4, 2 5, 3 6, 1 4))"
+
 
 def test_multipolygon_array_from_geobuffers():
     arr = ga.multipolygon().from_geobuffers(
@@ -336,11 +348,12 @@ def test_multipolygon_array_from_geobuffers():
         np.array([4.0, 5.0, 6.0, 4.0]),
     )
     assert len(arr) == 1
-    assert arr.as_wkt()[0].as_py() == 'MULTIPOLYGON (((1 4, 2 5, 3 6, 1 4)))'
+    assert arr.as_wkt()[0].as_py() == "MULTIPOLYGON (((1 4, 2 5, 3 6, 1 4)))"
+
 
 # Easier to test here because we have actual geoarrow arrays to parse
 def test_c_array_view():
-    arr = ga.array(['POLYGON ((0 0, 1 0, 0 1, 0 0))']).as_geoarrow(ga.polygon())
+    arr = ga.array(["POLYGON ((0 0, 1 0, 0 1, 0 0))"]).as_geoarrow(ga.polygon())
 
     cschema = lib.SchemaHolder()
     arr.type._export_to_c(cschema._addr())
@@ -355,24 +368,25 @@ def test_c_array_view():
 
     assert buffers[0] is None
 
-    assert buffer_arrays[1].shape == (2, )
+    assert buffer_arrays[1].shape == (2,)
     assert buffer_arrays[1][0] == 0
     assert buffer_arrays[1][1] == 1
 
-    assert buffer_arrays[2].shape == (2, )
+    assert buffer_arrays[2].shape == (2,)
     assert buffer_arrays[2][0] == 0
     assert buffer_arrays[2][1] == 4
 
-    assert buffer_arrays[3].shape == (4, )
+    assert buffer_arrays[3].shape == (4,)
     assert buffer_arrays[3][1] == 1
     assert buffer_arrays[3][3] == 0
 
-    assert buffer_arrays[4].shape == (4, )
+    assert buffer_arrays[4].shape == (4,)
     assert buffer_arrays[4][1] == 0
     assert buffer_arrays[4][3] == 0
 
+
 def test_c_array_view_interleaved():
-    arr = ga.array(['POLYGON ((0 0, 1 0, 0 1, 0 0))'])
+    arr = ga.array(["POLYGON ((0 0, 1 0, 0 1, 0 0))"])
     arr = arr.as_geoarrow(ga.polygon().with_coord_type(ga.CoordType.INTERLEAVED))
 
     cschema = lib.SchemaHolder()
@@ -388,15 +402,15 @@ def test_c_array_view_interleaved():
 
     assert buffers[0] is None
 
-    assert buffer_arrays[1].shape == (2, )
+    assert buffer_arrays[1].shape == (2,)
     assert buffer_arrays[1][0] == 0
     assert buffer_arrays[1][1] == 1
 
-    assert buffer_arrays[2].shape == (2, )
+    assert buffer_arrays[2].shape == (2,)
     assert buffer_arrays[2][0] == 0
     assert buffer_arrays[2][1] == 4
 
-    assert buffer_arrays[3].shape == (8, )
+    assert buffer_arrays[3].shape == (8,)
     assert buffer_arrays[3][0] == 0
     assert buffer_arrays[3][1] == 0
     assert buffer_arrays[3][2] == 1
