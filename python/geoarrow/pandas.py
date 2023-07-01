2085c01db2251781f553591d4490bb0f3bf16725
Dewey Dunnington <dewey@fishandwhistle.net>
2023-06-30 23:23:28 -0300

    more accessors wired up to compute functions

--- python/geoarrow/pandas.py
+++ python/geoarrow/pandas.py
@@ -1,17 +1,99 @@
-import pandas as pd
-import pyarrow as pa
-from . import pyarrow as ga
+import pandas as _pd
+import pyarrow as _pa
+from . import pyarrow as _ga
 
 
-@pd.api.extensions.register_series_accessor("geoarrow")
+@_pd.api.extensions.register_series_accessor("geoarrow")
 class GeoArrowAccessor:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
-    def as_geoarrow(self, type=None, coord_type=None):
-        array_or_chunked = ga.as_geoarrow(self._obj, type=type, coord_type=coord_type)
-        return pd.Series(
+    def _wrap_series(self, array_or_chunked):
+        return _pd.Series(
             array_or_chunked,
             index=self._obj.index,
-            dtype=pd.ArrowDtype(array_or_chunked.type),
+            dtype=_pd.ArrowDtype(array_or_chunked.type),
         )
+
+    def _obj_is_geoarrow(self):
+        return isinstance(self._obj.dtype, _pd.ArrowDtype) and isinstance(
+            self._obj.dtype.pyarrow_dtype, _ga.VectorType
+        )
+
+    def parse_all(self):
+        _ga.parse_all(self._obj)
+        return self._obj
+
+    def as_wkt(self):
+        return self._wrap_series(_ga.as_wkt(self._obj))
+
+    def as_wkb(self):
+        return self._wrap_series(_ga.as_wkb(self._obj))
+
+    def format_wkt(self, significant_digits=None, max_element_size_bytes=None):
+        if not self._obj_is_geoarrow():
+            raise TypeError("Can't format_wkt() a non-geoarrow Series")
+
+        array_or_chunked = _ga.format_wkt(
+            _pa.array(self._obj),
+            significant_digits=significant_digits,
+            max_element_size_bytes=max_element_size_bytes,
+        )
+        return self._wrap_series(array_or_chunked)
+
+    def format_wkb(self):
+        if not self._obj_is_geoarrow():
+            raise TypeError("Can't format_wkb() a non-geoarrow Series")
+
+        array_or_chunked = _ga.as_wkb(_pa.array(self._obj))
+        if isinstance(array_or_chunked, _pa.ChunkedArray):
+            storage = [chunk.storage for chunk in array_or_chunked.chunks]
+            return self._wrap_series(_pa.chunked_array(storage, _pa.binary()))
+        else:
+            return self._wrap_series(array_or_chunked.storage)
+
+    def as_geoarrow(self, type=None, coord_type=None):
+        array_or_chunked = _ga.as_geoarrow(self._obj, type=type, coord_type=coord_type)
+        return self._wrap_series(array_or_chunked)
+
+    def bounds(self):
+        array_or_chunked = _ga.box(self._obj)
+        if isinstance(array_or_chunked, _pa.ChunkedArray):
+            flattened = [chunk.flatten() for chunk in array_or_chunked.chunks]
+            seriesish = [
+                _pa.chunked_array(item, _pa.float64()) for item in zip(*flattened)
+            ]
+        else:
+            seriesish = array_or_chunked.flatten()
+
+        return _pd.DataFrame(
+            {
+                "xmin": seriesish[0],
+                "xmax": seriesish[1],
+                "ymin": seriesish[2],
+                "ymax": seriesish[3],
+            },
+            index=self._obj.index,
+        )
+
+    def total_bounds(self):
+        struct_scalar1 = _ga.box_agg(self._obj)
+        return _pd.DataFrame({k: [v] for k, v in struct_scalar1.as_py().items()})
+
+    def with_coord_type(self, coord_type):
+        return self._wrap_series(_ga.with_coord_type(self._obj, coord_type))
+
+    def with_edge_type(self, edge_type):
+        return self._wrap_series(_ga.with_edge_type(self._obj, edge_type))
+
+    def with_crs(self, crs, crs_type=None):
+        return self._wrap_series(_ga.with_crs(self._obj, crs=crs, crs_type=crs_type))
+
+    def with_dimensions(self, dimensions):
+        return self.with_dimensions(_ga.with_coord_type(self._obj, dimensions))
+
+    def with_geometry_type(self, geometry_type):
+        return self.with_geometry_type(_ga.with_coord_type(self._obj, geometry_type))
+
+    def point_coords(self, dimensions=None):
+        return self.point_coords(_ga.with_coord_type(self._obj, dimensions))
--- python/tests/test_geoarrow_pandas.py
+++ python/tests/test_geoarrow_pandas.py
@@ -1,13 +1,69 @@
+import pytest
+
 import pandas as pd
 import pyarrow as pa
 import geoarrow.pandas
 import geoarrow.pyarrow as ga
 
 
-def test_as_geoarrow():
-    series = pd.Series(["POINT (0 1)", "POINT (2 3)"])
-    ga_series = series.geoarrow.as_geoarrow()
-    assert isinstance(ga_series, pd.Series)
+def test_parse_all():
+    series = pd.Series(["POINT (0 1)"])
+    assert series.geoarrow.parse_all() is series
+    with pytest.raises(ValueError):
+        pd.Series(["NOT WKT"]).geoarrow.parse_all()
 
-    arr = ga.array(ga_series)
-    assert arr == ga.as_geoarrow(["POINT (0 1)", "POINT (2 3)"])
+
+def test_as_wkt():
+    ga_series = pd.Series(["POINT (0 1)"]).geoarrow.as_wkt()
+    assert isinstance(ga_series.dtype.pyarrow_dtype, ga.WktType)
+
+
+def test_as_wkb():
+    ga_series = pd.Series(["POINT (0 1)"]).geoarrow.as_wkb()
+    assert isinstance(ga_series.dtype.pyarrow_dtype, ga.WkbType)
+
+
+def test_format_wkt():
+    with pytest.raises(TypeError):
+        pd.Series(["POINT (0 1)"]).geoarrow.format_wkt()
+
+    ga_series = pd.Series(["POINT (0 1)"]).geoarrow.as_geoarrow().geoarrow.format_wkt()
+    assert ga_series.dtype.pyarrow_dtype == pa.utf8()
+
+
+def test_format_wkb():
+    with pytest.raises(TypeError):
+        pd.Series(["POINT (0 1)"]).geoarrow.format_wkb()
+
+    ga_series = pd.Series(["POINT (0 1)"]).geoarrow.as_geoarrow().geoarrow.format_wkb()
+    assert ga_series.dtype.pyarrow_dtype == pa.binary()
+
+    # Currently handles ChunkedArray explicitly
+    chunked = pa.chunked_array([ga.array(["POINT (0 1)"])])
+    ga_series = pd.Series(
+        chunked, dtype=pd.ArrowDtype(chunked.type)
+    ).geoarrow.format_wkb()
+    assert ga_series.dtype.pyarrow_dtype == pa.binary()
+
+
+def test_as_geoarrow():
+    ga_series = pd.Series(["POINT (0 1)"]).geoarrow.as_geoarrow()
+    assert isinstance(ga_series.dtype.pyarrow_dtype, ga.PointType)
+
+
+def test_bounds():
+    df = pd.Series(["POINT (0 1)"]).geoarrow.bounds()
+    assert isinstance(df, pd.DataFrame)
+    assert df.xmin[0] == 0
+    assert df.ymin[0] == 1
+    assert df.xmax[0] == 0
+    assert df.ymax[0] == 1
+
+
+def test_total_bounds():
+    df = pd.Series(["POINT (0 1)"]).geoarrow.total_bounds()
+    assert isinstance(df, pd.DataFrame)
+    assert df.xmin[0] == 0
+    assert df.ymin[0] == 1
+    assert df.xmax[0] == 0
+    assert df.ymax[0] == 1
