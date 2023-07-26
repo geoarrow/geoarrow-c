8c16c09dce5f0baa20af063c996469ab5855fad0
Dewey Dunnington <dewey@fishandwhistle.net>
2023-07-25 22:28:21 -0300

    move indexing methods into the GeoDataset class

--- python/geoarrow/dataset.py
+++ python/geoarrow/dataset.py
@@ -8,61 +8,6 @@ from . import pyarrow as _ga
 from .pyarrow._kernel import Kernel
 
 
-def _index_fragment(fragment, column):
-    scanner = fragment.scanner(columns=[column])
-    reader = scanner.to_reader()
-    kernel = Kernel.box_agg(reader.schema.types[0])
-    for batch in reader:
-        kernel.push(batch.column(0))
-    return kernel.finish()
-
-
-def _index_fragments(fragments, columns, num_threads=None):
-    columns = list(columns)
-    if num_threads is None:
-        num_threads = _pa.cpu_count()
-
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = []
-        for column in columns:
-            for fragment in fragments:
-                future = executor.submit(_index_fragment, fragment, column)
-                futures.append(future)
-
-        wait(futures)
-
-        num_fragments = len(futures) // len(columns)
-        results = []
-        for i, column in enumerate(columns):
-            results.append(
-                [futures[i * num_fragments + j].result() for j in range(num_fragments)]
-            )
-
-        metadata = [_pa.array(range(num_fragments))]
-        result_arrays = [_pa.concat_arrays(result) for result in results]
-        return _pa.table(metadata + result_arrays, names=["_fragment_index"] + columns)
-
-
-def _index_box_intersects(index, box, columns):
-    xmin, xmax, ymin, ymax = box.as_py().values()
-    expressions = []
-    for col in columns:
-        expr = (
-            (_compute.field(col, "xmin") <= xmax)
-            & (_compute.field(col, "xmax") >= xmin)
-            & (_compute.field(col, "ymin") <= ymax)
-            & (_compute.field(col, "ymax") >= ymin)
-        )
-        expressions.append(expr)
-
-    expr = expressions[0]
-    for i in range(1, len(expressions)):
-        expr = expr | expressions[i]
-
-    result = _ds.dataset(index).filter(expr).to_table()
-    return result.column(0)
-
-
 class GeoDataset:
     def __init__(self, parent, *args, geometry_columns=None, **kwargs) -> None:
         self._parent = None
@@ -107,7 +52,7 @@ class GeoDataset:
 
     def index_fragments(self, num_threads=None):
         if self._index is None:
-            self._index = _index_fragments(
+            self._index = GeoDataset._index_fragments(
                 self.get_fragments(), self.geometry_columns, num_threads=num_threads
             )
 
@@ -117,11 +62,11 @@ class GeoDataset:
         if isinstance(target, str):
             target = [target]
         target_box = _ga.box_agg(target)
-        maybe_intersects = _index_box_intersects(
+        maybe_intersects = GeoDataset._index_box_intersects(
             self.index_fragments(), target_box, self.geometry_columns
         )
         fragment_indices = [scalar.as_py() for scalar in maybe_intersects]
-        fragments = list(self.get_fragments())
+        fragments = self.get_fragments()
         fragments_filtered = [fragments[i] for i in fragment_indices]
 
         if isinstance(self._parent, _ds.FileSystemDataset):
@@ -131,6 +76,68 @@ class GeoDataset:
         else:
             tables = [fragment.to_table() for fragment in fragments_filtered]
             return _ds.InMemoryDataset(tables)
+
+    @staticmethod
+    def _index_fragment(fragment, column):
+        scanner = fragment.scanner(columns=[column])
+        reader = scanner.to_reader()
+        kernel = Kernel.box_agg(reader.schema.types[0])
+        for batch in reader:
+            kernel.push(batch.column(0))
+        return kernel.finish()
+
+    @staticmethod
+    def _index_fragments(fragments, columns, num_threads=None):
+        columns = list(columns)
+        if num_threads is None:
+            num_threads = _pa.cpu_count()
+
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            futures = []
+            for column in columns:
+                for fragment in fragments:
+                    future = executor.submit(
+                        GeoDataset._index_fragment, fragment, column
+                    )
+                    futures.append(future)
+
+            wait(futures)
+
+            num_fragments = len(futures) // len(columns)
+            results = []
+            for i, column in enumerate(columns):
+                results.append(
+                    [
+                        futures[i * num_fragments + j].result()
+                        for j in range(num_fragments)
+                    ]
+                )
+
+            metadata = [_pa.array(range(num_fragments))]
+            result_arrays = [_pa.concat_arrays(result) for result in results]
+            return _pa.table(
+                metadata + result_arrays, names=["_fragment_index"] + columns
+            )
+
+    @staticmethod
+    def _index_box_intersects(index, box, columns):
+        xmin, xmax, ymin, ymax = box.as_py().values()
+        expressions = []
+        for col in columns:
+            expr = (
+                (_compute.field(col, "xmin") <= xmax)
+                & (_compute.field(col, "xmax") >= xmin)
+                & (_compute.field(col, "ymin") <= ymax)
+                & (_compute.field(col, "ymax") >= ymin)
+            )
+            expressions.append(expr)
+
+        expr = expressions[0]
+        for i in range(1, len(expressions)):
+            expr = expr | expressions[i]
+
+        result = _ds.dataset(index).filter(expr).to_table()
+        return result.column(0)
 
 
 class ParquetRowGroupGeoDataset(GeoDataset):
