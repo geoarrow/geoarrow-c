84997a1fbef823701c3912e32de49ae9edfbaf30
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-06 23:14:17 -0300

    make sure tests pass with both printers

--- src/geoarrow/double_print.c
+++ src/geoarrow/double_print.c
@@ -14,7 +14,21 @@ int64_t GeoArrowPrintDouble(double f, uint32_t precision, char* result) {
 #include <stdio.h>
 
 int64_t GeoArrowPrintDouble(double f, uint32_t precision, char* result) {
-  snprintf(result, 128, "%.*g", precision, f);
+  int64_t n_chars = snprintf(result, 128, "%0.*f", precision, f);
+
+  // Strip trailing zeroes + decimal
+  for (int64_t i = n_chars - 1; i >= 0; i--) {
+    if (result[i] == '0') {
+      n_chars--;
+    } else if (result[i] == '.') {
+      n_chars--;
+      break;
+    } else {
+      break;
+    }
+  }
+
+  return n_chars;
 }
 
 #endif
