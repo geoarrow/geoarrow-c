d287550953d7ee94cf2081c568f5b8ffd9d28a80
Dewey Dunnington <dewey@voltrondata.com>
2023-09-07 12:59:58 -0300

    maybe docs

--- ci/scripts/build-docs.sh
+++ ci/scripts/build-docs.sh
@@ -51,7 +51,7 @@ main() {
    # pip install . doesn't quite work with the setuptools available on the
    # ubuntu docker image...python -m build works I think because it sets up
    # a virtualenv
-   pushd python
+   pushd python/geoarrow-c
    rm -rf dist
    python3 -m build --wheel
    pip3 install dist/geoarrow-*.whl
