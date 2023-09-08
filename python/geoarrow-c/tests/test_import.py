e4a00760e7059453a54b74ed6d82f37280c35db8
Dewey Dunnington <dewey@dunnington.ca>
2023-09-08 16:26:42 -0300

    Build Python wheels for geoarrow-c (#53)
    * give the wheels thing a go
    
    * maybe setup subdirectory
    
    * maybe fix install on Python 3.8
    
    * See if we can test those wheels
    
    * also make numpy optional
    
    * don't try to build anything for testing
    
    * restrict wheel building workflow
    
    * add a note

---
+++ python/geoarrow-c/tests/test_import.py
@@ -0,0 +1,11 @@
+import geoarrow.c as ga
+
+# This is mostly a naive sanity check that the native extension can be loaded
+# on platforms where there is no pyarrow.
+
+def test_enums():
+    assert ga.CoordType.UNKNOWN == 0
+    assert ga.CrsType.UNKNOWN == 1
+    assert ga.Dimensions.UNKNOWN == 0
+    assert ga.EdgeType.PLANAR == 0
+    assert ga.GeometryType.GEOMETRY == 0
