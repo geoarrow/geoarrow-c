f8128274a20f272a2833831a85b439d2f846ad22
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-10 12:04:19 +0200

    sketch type math

--- r/geoarrow/R/type.R
+++ r/geoarrow/R/type.R
@@ -1,4 +1,75 @@
 
+na_extension_wkb <- function(crs = NULL, edge_type = "PLANAR") {
+  na_extension_geoarrow_internal(
+    enum$Type$WKB,
+    crs = crs,
+    edge_type = edge_type
+  )
+}
+
+na_extension_wkt <- function(crs = NULL, edge_type = "PLANAR") {
+  na_extension_geoarrow_internal(
+    enum$Type$WKT,
+    crs = crs,
+    edge_type = edge_type
+  )
+}
+
+na_extension_large_wkb <- function(crs = NULL, edge_type = "PLANAR") {
+  na_extension_geoarrow_internal(
+    enum$Type$LARGE_WKB,
+    crs = crs,
+    edge_type = edge_type
+  )
+}
+
+na_extension_large_wkt <- function(crs = NULL, edge_type = "PLANAR") {
+  na_extension_geoarrow_internal(
+    enum$Type$LARGE_WKT,
+    crs = crs,
+    edge_type = edge_type
+  )
+}
+
+na_extension_geoarrow <- function(geometry_type, dimensions = "XY",
+                                  coord_type = "SEPARATE",
+                                  crs = NULL, edge_type = "PLANAR") {
+  geometry_type <- enum_value_scalar(geometry_type, "GeometryType")
+  dimensions <- enum_value_scalar(dimensions, "Dimensions")
+  coord_type <- enum_value_scalar(coord_type, "CoordType")
+
+  type_id <- .Call(geoarrow_c_make_type, geometry_type, dimensions, coord_type)
+  na_extension_geoarrow_internal(type_id, crs = crs, edge_type = edge_type)
+}
+
+na_extension_geoarrow_internal <- function(type_id, crs = NULL, edge_type = "PLANAR") {
+  crs <- sanitize_crs(crs)
+  edge_type <- enum_value_scalar(edge_type, "EdgeType")
+  schema <- nanoarrow::nanoarrow_allocate_schema()
+  .Call(
+    geoarrow_c_schema_init_extension,
+    type_id,
+    crs$type,
+    crs$crs,edge_type
+  )
+
+  schema
+}
+
+sanitize_crs <- function(crs = NULL) {
+  if (is.null(crs)) {
+    return(list(crs_type = enum$CrsType$NONE, crs = NA_character_))
+  }
+
+  crs_projjson <- wk::wk_crs_projjson(crs)
+  if (identical(crs_projjson, NA_character_)) {
+    crs_type <- enum$CrsType$UNKNOWN
+    return(list(crs_type = enum$CrsType$UNKNOWN, crs = crs))
+  }
+
+  list(crs_type = enum$CrsType$UNKNOWN, crs = crs_projjson)
+}
+
 enum_value <- function(x, enum_name) {
   values <- unlist(enum[[enum_name]])
   if (is.character(x)) {
@@ -18,7 +89,23 @@ enum_label <- function(x, enum_name) {
   }
 }
 
+enum_value_scalar <- function(x, enum_name) {
+  x_clean <- enum_value(x, enum_name)
+  if (length(x_clean) != 1 || is.na(x_clean)) {
+    stop(sprintf("%s is not a valid enum label or value for %s", x, enum_name))
+  }
+
+  x_clean
+}
+
 enum <- list(
+  Type = list(
+    UNINITIALIZED = 0L,
+    WKB = 100001L,
+    LARGE_WKB = 100002L,
+    WKT = 100003L,
+    LARGE_WKT = 100004L
+  ),
   GeometryType = list(
     GEOMETRY = 0L,
     POINT = 1L,
@@ -45,9 +132,9 @@ enum <- list(
     NONE = 0L,
     UNKNOWN = 1L,
     PROJJSON = 2L
+  ),
+  EdgeType = list(
+    PLANAR = 0L,
+    SPHERICAL = 1L
   )
 )
-
-stop_invalid_enum <- function(x, enum_name) {
-  stop(sprintf("%s is not a valid enum label or value for %s", x, enum_name))
-}
--- r/geoarrow/tests/testthat/test-type.R
+++ r/geoarrow/tests/testthat/test-type.R
@@ -1,4 +1,8 @@
 
+test_that("serialized extensions work", {
+
+})
+
 test_that("enum matcher works", {
   expect_identical(
     enum_value(c("GEOMETRY", "MULTIPOINT", "NOT VALID"), "GeometryType"),
@@ -20,5 +24,17 @@ test_that("enum labeller works", {
   expect_identical(
     enum_label(c(0L, 4L, 9L), "GeometryType"),
     c("GEOMETRY", "MULTIPOINT", NA_character_)
+  )
+})
+
+test_that("enum_scalar matcher errors for bad values", {
+  expect_error(
+    enum_value_scalar("NOT VALID", "GeometryType"),
+    "NOT VALID is not a valid enum label or value for GeometryType"
+  )
+
+  expect_error(
+    enum_value_scalar(10, "GeometryType"),
+    "10 is not a valid enum label or value for GeometryType"
   )
 })
