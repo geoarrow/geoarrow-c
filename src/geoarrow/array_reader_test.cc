a80f233433e36f22de2db7b004deb3e2daf04b24
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-17 13:31:28 +0200

    with wkb test

--- src/geoarrow/array_reader_test.cc
+++ src/geoarrow/array_reader_test.cc
@@ -49,8 +49,46 @@ TEST(ArrayReaderTest, ArrayReaderTestVisitWKT) {
 }
 
 TEST(ArrayReaderTest, ArrayReaderTestVisitWKB) {
+  struct ArrowSchema schema;
+  struct ArrowArray array;
+  enum GeoArrowType type = GEOARROW_TYPE_WKB;
+
+  std::basic_string<uint8_t> point({0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                    0x00, 0x00, 0x00, 0x00, 0x3e, 0x40, 0x00,
+                                    0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x40});
+  struct ArrowBufferView point_view;
+  point_view.data.as_uint8 = point.data();
+  point_view.size_bytes = point.size();
+
+  // Build the array for [POINT (30 10), null]
+  ASSERT_EQ(GeoArrowSchemaInit(&schema, type), GEOARROW_OK);
+  ASSERT_EQ(ArrowArrayInitFromSchema(&array, &schema, nullptr), GEOARROW_OK);
+  ASSERT_EQ(ArrowArrayStartAppending(&array), GEOARROW_OK);
+
+  ASSERT_EQ(ArrowArrayAppendBytes(&array, point_view), GEOARROW_OK);
+  ASSERT_EQ(ArrowArrayAppendNull(&array, 1), GEOARROW_OK);
+
+  ASSERT_EQ(ArrowArrayFinishBuildingDefault(&array, nullptr), GEOARROW_OK);
+
+  // Set the array view
+  struct GeoArrowArrayView array_view;
+  EXPECT_EQ(GeoArrowArrayViewInitFromType(&array_view, type), GEOARROW_OK);
+  EXPECT_EQ(GeoArrowArrayViewSetArray(&array_view, &array, nullptr), GEOARROW_OK);
+
+  // Check its contents
+  WKXTester tester;
   struct GeoArrowArrayReader reader;
   ASSERT_EQ(GeoArrowArrayReaderInit(&reader), GEOARROW_OK);
+  EXPECT_EQ(GeoArrowArrayReaderVisit(&reader, &array_view, 0, array.length,
+                                     tester.WKTVisitor()),
+            GEOARROW_OK);
+  auto values = tester.WKTValues("<null value>");
+  ASSERT_EQ(values.size(), 2);
+  EXPECT_EQ(values[0], "POINT (30 10)");
+  EXPECT_EQ(values[1], "<null value>");
+
+  schema.release(&schema);
+  array.release(&array);
   GeoArrowArrayReaderReset(&reader);
 }
 
