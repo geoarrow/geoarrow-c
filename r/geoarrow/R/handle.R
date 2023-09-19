89d92d1fdc417a137870c9fa1b956fdb4478cbba
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-19 15:12:18 +0200

    writer support

--- r/geoarrow/R/handle.R
+++ r/geoarrow/R/handle.R
@@ -12,3 +12,13 @@ geoarrow_handle <- function(x, handler, size = NA_integer_) {
 
   .Call(geoarrow_c_handle_stream, data, handler)
 }
+
+geoarrow_writer <- function(schema) {
+  array_out <- nanoarrow::nanoarrow_allocate_array()
+  schema <- nanoarrow::as_nanoarrow_schema(schema)
+  nanoarrow::nanoarrow_array_set_schema(array_out, schema, validate = FALSE)
+  wk::new_wk_handler(
+    .Call(geoarrow_c_writer_new, schema, array_out),
+    "geoarrow_writer"
+  )
+}
--- r/geoarrow/src/init.c
+++ r/geoarrow/src/init.c
@@ -1,21 +1,22 @@
 #define R_NO_REMAP
 #include <R.h>
-#include <Rinternals.h>
 #include <R_ext/Rdynload.h>
+#include <Rinternals.h>
 
 SEXP geoarrow_c_handle_stream(SEXP data, SEXP handler_xptr);
 SEXP geoarrow_c_make_type(SEXP geometry_type_sexp, SEXP dimensions_sexp,
                           SEXP coord_type_sexp);
 SEXP geoarrow_c_schema_init_extension(SEXP schema_xptr, SEXP type_sexp);
+SEXP geoarrow_c_writer_new(SEXP schema_xptr, SEXP array_out_xptr);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"geoarrow_c_handle_stream", (DL_FUNC) &geoarrow_c_handle_stream, 2},
-  {"geoarrow_c_make_type", (DL_FUNC) &geoarrow_c_make_type, 3},
-  {"geoarrow_c_schema_init_extension", (DL_FUNC) &geoarrow_c_schema_init_extension, 2},
-  {NULL, NULL, 0}
-};
+    {"geoarrow_c_handle_stream", (DL_FUNC)&geoarrow_c_handle_stream, 2},
+    {"geoarrow_c_make_type", (DL_FUNC)&geoarrow_c_make_type, 3},
+    {"geoarrow_c_schema_init_extension", (DL_FUNC)&geoarrow_c_schema_init_extension, 2},
+    {"geoarrow_c_writer_new", (DL_FUNC)&geoarrow_c_writer_new, 2},
+    {NULL, NULL, 0}};
 
-void R_init_geoarrow(DllInfo *dll) {
+void R_init_geoarrow(DllInfo* dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
 }
