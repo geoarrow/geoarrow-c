cefd40698e293a369894b95fed932e2a171f3592
Dewey Dunnington <dewey@fishandwhistle.net>
2023-09-21 19:56:43 +0200

    note about future

--- r/geoarrow/R/sf-compat.R
+++ r/geoarrow/R/sf-compat.R
@@ -4,3 +4,7 @@
 infer_nanoarrow_schema.sfc <- function(x, ...) {
   geoarrow_infer_schema_default(x)
 }
+
+# Eventually we can add specializations for as_geoarrow_array() based on
+# st_as_grob(), which is very fast and generates lengths + a column-major
+# matrix full of buffers we can provide views into.
