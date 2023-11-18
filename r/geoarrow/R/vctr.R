2257e8813f4afd716ef84968149f2a81e842898d
Dewey Dunnington <dewey@fishandwhistle.net>
2023-11-17 22:00:29 -0400

    basic tests

--- r/geoarrow/R/vctr.R
+++ r/geoarrow/R/vctr.R
@@ -9,13 +9,17 @@ as_geoarrow_vctr <- function(x, ..., schema = NULL) {
   new_geoarrow_vctr(chunks, stream$get_schema())
 }
 
-new_geoarrow_vctr <- function(chunks, schema) {
+new_geoarrow_vctr <- function(chunks, schema, indices = NULL) {
   offsets <- .Call(geoarrow_c_vctr_chunk_offsets, chunks)
+  if (is.null(indices)) {
+    indices <- seq_len(offsets[length(offsets)])
+  }
+
   structure(
-    seq_len(offsets[length(offsets)]),
+    indices,
     schema = schema,
     chunks = chunks,
-    chunk_offsets = offsets,
+    offsets = offsets,
     class = c("geoarrow_vctr", "wk_vctr")
   )
 }
@@ -30,6 +34,7 @@ as_geoarrow_array_stream.geoarrow_vctr <- function(x, ..., schema = NULL) {
   as_nanoarrow_array_stream.geoarrow_vctr(x, ..., schema = NULL)
 }
 
+#' @importFrom nanoarrow as_nanoarrow_array_stream
 #' @export
 as_nanoarrow_array_stream.geoarrow_vctr <- function(x, ..., schema = NULL) {
   slice <- vctr_as_slice(x)
@@ -37,7 +42,39 @@ as_nanoarrow_array_stream.geoarrow_vctr <- function(x, ..., schema = NULL) {
     stop("Can't resolve non-slice geoarrow_vctr to nanoarrow_array_stream")
   }
 
-  chunk1
+  x_schema <- attr(x, "schema")
+
+  # Zero-size slice can be an array stream with zero batches
+  if (slice[2] == 0) {
+    return(nanoarrow::basic_array_stream(list(), schema = x_schema))
+  }
+
+  # Full slice doesn't need slicing logic
+  offsets <- attr(x, "offsets")
+  batches <- attr(x, "chunks")
+  if (slice[1] == 0 && slice[2] == max(offsets)) {
+    return(
+      nanoarrow::basic_array_stream(
+        batches,
+        schema = x_schema,
+        validate = FALSE
+      )
+    )
+  }
+
+  # Calculate first and last slice information
+  first_index <- slice[1]
+  last_index <- slice[1] + slice[2]
+  first_chunk_index <- vctr_resolve_chunk(first_index, offsets)
+  last_chunk_index <- vctr_resolve_chunk(last_index, offsets)
+
+  first_chunk_offset <- first_index - offsets[first_chunk_index]
+  first_chunk_length <- offsets[first_chunk_index + 1] - first_index
+  last_chunk_offset <- 0
+  last_chunk_length <- last_index - offsets[last_chunk_index]
+
+  # Calculate first and last slices
+
 
   nanoarrow::basic_array_stream(
     attr(x, "chunks")
--- r/geoarrow/tests/testthat/test-vctr.R
+++ r/geoarrow/tests/testthat/test-vctr.R
@@ -1,4 +1,30 @@
 
+test_that("as_nanoarrow_array_stream() generates an empty stream for empty slice", {
+  vctr <- new_geoarrow_vctr(list(), na_extension_wkt())
+  stream <- nanoarrow::as_nanoarrow_array_stream(vctr)
+  schema_out <- stream$get_schema()
+  expect_identical(schema_out$format, "u")
+  expect_identical(nanoarrow::collect_array_stream(stream), list())
+})
+
+test_that("as_nanoarrow_array_stream() generates identical stream for identity slice", {
+  array <- as_geoarrow_array("POINT (0 1)")
+  vctr <- new_geoarrow_vctr(list(array), infer_nanoarrow_schema(array))
+
+  stream <- nanoarrow::as_nanoarrow_array_stream(vctr)
+  schema_out <- stream$get_schema()
+  expect_identical(schema_out$format, "u")
+
+  collected <- nanoarrow::collect_array_stream(stream)
+  expect_length(collected, 1)
+  expect_identical(
+    nanoarrow::convert_buffer(array$buffers[[3]]),
+    "POINT (0 1)"
+  )
+})
+
+
+
 test_that("slice detector works", {
   expect_identical(
     vctr_as_slice(logical()),
