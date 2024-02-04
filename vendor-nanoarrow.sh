9a5dbbb5515c3927dcccae7a92fa2ca2cd229885
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-04 10:40:55 -0400

    update to 0.4.0

--- vendor-nanoarrow.sh
+++ vendor-nanoarrow.sh
@@ -1,7 +1,7 @@
 
 for f in nanoarrow.h nanoarrow.c; do
   curl -L \
-    https://raw.githubusercontent.com/paleolimbot/arrow-nanoarrow/unknown-flags/dist/$f \
+    https://raw.githubusercontent.com/apache/arrow-nanoarrow/apache-arrow-nanoarrow-0.4.0/dist/$f \
     -o src/geoarrow/$f
 done
 
