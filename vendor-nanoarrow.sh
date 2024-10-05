85a2addc309bef9d2d87cbd7f2f47dbb858def50
Dewey Dunnington <dewey@fishandwhistle.net>
2024-10-04 23:13:58 -0500

    update vendored nanoarrow

--- vendor-nanoarrow.sh
+++ vendor-nanoarrow.sh
@@ -1,15 +1,37 @@
 
-for f in nanoarrow.h nanoarrow.c; do
-  curl -L \
-    https://raw.githubusercontent.com/apache/arrow-nanoarrow/apache-arrow-nanoarrow-0.5.0/dist/$f \
-    -o src/geoarrow/$f
-done
 
-sed -i.bak \
-  -e 's|// #define NANOARROW_NAMESPACE YourNamespaceHere|// When testing we use nanoarrow.h, but geoarrow_config.h will not exist in bundled\
+
+main() {
+  local -r repo_url="https://github.com/paleolimbot/arrow-nanoarrow"
+  # Check releases page: https://github.com/apache/arrow-nanoarrow/releases/
+  local -r commit_sha=ec7a2a1a43922ef4c5b025a22194d8f121507fb9
+
+  echo "Fetching $commit_sha from $repo_url"
+  SCRATCH=$(mktemp -d)
+  trap 'rm -rf "$SCRATCH"' EXIT
+
+  local -r tarball="$SCRATCH/nanoarrow.tar.gz"
+  wget -O "$tarball" "$repo_url/archive/$commit_sha.tar.gz"
+  tar --strip-components 1 -C "$SCRATCH" -xf "$tarball"
+
+  # Build the bundle
+  python "${SCRATCH}/ci/scripts/bundle.py" \
+      --include-output-dir=src/geoarrow \
+      --source-output-dir=src/geoarrow \
+      --header-namespace=
+
+  rm src/geoarrow/nanoarrow.hpp
+
+  sed -i.bak \
+    -e 's|// #define NANOARROW_NAMESPACE YourNamespaceHere|// When testing we use nanoarrow.h, but geoarrow_config.h will not exist in bundled\
 // mode. In the tests we just have to make sure geoarrow.h is always included first.\
 #if !defined(GEOARROW_CONFIG_H_INCLUDED)\
 #include "geoarrow_config.h"\
 #endif|' \
-  src/geoarrow/nanoarrow.h
-rm src/geoarrow/nanoarrow.h.bak
+    src/geoarrow/nanoarrow.h
+  rm src/geoarrow/nanoarrow.h.bak
+}
+
+# git clone https://github.com/paleolimbot/nanoarrow
+
+main
