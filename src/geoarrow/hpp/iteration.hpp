706ca1c0f98169334167ba10cdc74a30bd774ac7
Dewey Dunnington <dewey@wherobots.com>
2025-01-02 16:51:12 -0600

    maybe implement array

--- src/geoarrow/hpp/iteration.hpp
+++ src/geoarrow/hpp/iteration.hpp
@@ -12,85 +12,14 @@ namespace geoarrow {
 
 namespace array {
 
-struct XY : public std::array<double, 2> {
-  double x() const { return at(0); }
-  double y() const { return at(1); }
-  double z() const { return NAN; }
-  double m() const { return NAN; }
-};
-
-struct XYZ : public std::array<double, 3> {
-  double x() const { return at(0); }
-  double y() const { return at(1); }
-  double z() const { return at(2); }
-  double m() const { return NAN; }
-};
-
-struct XYM : public std::array<double, 3> {
-  double x() const { return at(0); }
-  double y() const { return at(1); }
-  double z() const { return NAN; }
-  double m() const { return at(2); }
-};
-
-struct XYZM : public std::array<double, 4> {
-  double x() const { return at(0); }
-  double y() const { return at(1); }
-  double z() const { return at(2); }
-  double m() const { return at(3); }
-};
-
-struct BoxXY : public std::array<double, 4> {
-  double xmin() const { return at(0); }
-  double ymin() const { return at(1); }
-  double zmin() const { return INFINITY; }
-  double mmin() const { return INFINITY; }
-  double xmax() const { return at(2); }
-  double ymax() const { return at(3); }
-  double zmax() const { return -INFINITY; }
-  double mmax() const { return -INFINITY; }
-};
-
-struct BoxXYZ : public std::array<double, 6> {
-  double xmin() const { return at(0); }
-  double ymin() const { return at(1); }
-  double zmin() const { return at(2); }
-  double mmin() const { return INFINITY; }
-  double xmax() const { return at(3); }
-  double ymax() const { return at(4); }
-  double zmax() const { return at(5); }
-  double mmax() const { return -INFINITY; }
-};
-
-struct BoxXYM : public std::array<double, 6> {
-  double xmin() const { return at(0); }
-  double ymin() const { return at(1); }
-  double zmin() const { return INFINITY; }
-  double mmin() const { return at(2); }
-  double xmax() const { return at(3); }
-  double ymax() const { return at(4); }
-  double zmax() const { return -INFINITY; }
-  double mmax() const { return at(5); }
-};
-
-struct BoxXYZM : public std::array<double, 8> {
-  double xmin() const { return at(0); }
-  double ymin() const { return at(1); }
-  double zmin() const { return at(2); }
-  double mmin() const { return at(3); }
-  double xmax() const { return at(4); }
-  double ymax() const { return at(5); }
-  double zmax() const { return at(7); }
-  double mmax() const { return at(8); }
-};
-
+namespace internal {
 template <typename CoordSequence>
 class CoordSequenceIterator {
   const CoordSequence& outer_;
-  int64_t i_;
+  uint32_t i_;
 
  public:
-  explicit CoordSequenceIterator(const CoordSequence& outer, int64_t i = 0)
+  explicit CoordSequenceIterator(const CoordSequence& outer, uint32_t i = 0)
       : outer_(outer), i_(i) {}
   CoordSequenceIterator& operator++() {
     i_++;
@@ -139,18 +68,104 @@ class NestedIterator {
   using difference_type = int64_t;
   using value_type = typename Nested::child_type&;
 };
+}  // namespace internal
+
+struct XY : public std::array<double, 2> {
+  double x() const { return at(0); }
+  double y() const { return at(1); }
+  double z() const { return NAN; }
+  double m() const { return NAN; }
+};
+
+struct XYZ : public std::array<double, 3> {
+  double x() const { return at(0); }
+  double y() const { return at(1); }
+  double z() const { return at(2); }
+  double m() const { return NAN; }
+};
+
+struct XYM : public std::array<double, 3> {
+  double x() const { return at(0); }
+  double y() const { return at(1); }
+  double z() const { return NAN; }
+  double m() const { return at(2); }
+};
+
+struct XYZM : public std::array<double, 4> {
+  double x() const { return at(0); }
+  double y() const { return at(1); }
+  double z() const { return at(2); }
+  double m() const { return at(3); }
+};
+
+struct BoxXY : public std::array<double, 4> {
+  using bound_type = XY;
+  double xmin() const { return at(0); }
+  double ymin() const { return at(1); }
+  double zmin() const { return INFINITY; }
+  double mmin() const { return INFINITY; }
+  double xmax() const { return at(2); }
+  double ymax() const { return at(3); }
+  double zmax() const { return -INFINITY; }
+  double mmax() const { return -INFINITY; }
+  bound_type lower_bound() { return {xmin(), ymin()}; }
+  bound_type upper_bound() { return {xmax(), ymax()}; }
+};
+
+struct BoxXYZ : public std::array<double, 6> {
+  using bound_type = XYZ;
+  double xmin() const { return at(0); }
+  double ymin() const { return at(1); }
+  double zmin() const { return at(2); }
+  double mmin() const { return INFINITY; }
+  double xmax() const { return at(3); }
+  double ymax() const { return at(4); }
+  double zmax() const { return at(5); }
+  double mmax() const { return -INFINITY; }
+  bound_type lower_bound() { return {xmin(), ymin(), zmin()}; }
+  bound_type upper_bound() { return {xmax(), ymax(), zmax()}; }
+};
+
+struct BoxXYM : public std::array<double, 6> {
+  using bound_type = XYM;
+  double xmin() const { return at(0); }
+  double ymin() const { return at(1); }
+  double zmin() const { return INFINITY; }
+  double mmin() const { return at(2); }
+  double xmax() const { return at(3); }
+  double ymax() const { return at(4); }
+  double zmax() const { return -INFINITY; }
+  double mmax() const { return at(5); }
+  bound_type lower_bound() { return {mmin(), ymin(), mmin()}; }
+  bound_type upper_bound() { return {xmax(), ymax(), mmax()}; }
+};
+
+struct BoxXYZM : public std::array<double, 8> {
+  using bound_type = XYZM;
+  double xmin() const { return at(0); }
+  double ymin() const { return at(1); }
+  double zmin() const { return at(2); }
+  double mmin() const { return at(3); }
+  double xmax() const { return at(4); }
+  double ymax() const { return at(5); }
+  double zmax() const { return at(7); }
+  double mmax() const { return at(8); }
+  bound_type lower_bound() { return {xmin(), ymin(), zmin(), mmin()}; }
+  bound_type upper_bound() { return {xmax(), ymax(), zmax(), mmax()}; }
+};
 
 template <typename Coord>
 struct CoordSequence {
   using coord_type = Coord;
+  static constexpr bool is_sequence = true;
   static constexpr uint32_t coord_size = Coord().size();
   uint32_t offset;
   uint32_t length;
   struct GeoArrowCoordView* coord_view;
-  double* values[coord_size];
+  const double* values[coord_size];
   uint32_t stride;
 
-  Coord operator[](int64_t i) const {
+  Coord operator[](uint32_t i) const {
     Coord out;
     for (size_t j = 0; j < out.size(); j++) {
       out[j] = GEOARROW_COORD_VIEW_VALUE(coord_view, offset + i, j);
@@ -160,20 +175,18 @@ struct CoordSequence {
 
   int64_t size() const { return length; }
 
-  CoordSequenceIterator<CoordSequence> begin() const {
-    return CoordSequenceIterator<CoordSequence>(*this);
-  }
-  CoordSequenceIterator<CoordSequence> end() const {
-    return CoordSequenceIterator<CoordSequence>(*this, length);
-  }
+  using Iterator = internal::CoordSequenceIterator<CoordSequence>;
+  Iterator begin() const { return Iterator(*this); }
+  Iterator end() const { return Iterator(*this, length); }
 };
 
 template <typename T>
 struct Nested {
   using child_type = T;
+  static constexpr bool is_sequence = false;
 
-  int64_t offset;
-  int64_t length;
+  uint32_t offset;
+  uint32_t length;
   const int32_t* offsets;
   T child;
 
@@ -182,14 +195,92 @@ struct Nested {
     UpdateChild(child_p, 0);
   }
 
-  void UpdateChild(T* child_p, int64_t i) const {
+  void UpdateChild(T* child_p, uint32_t i) const {
     int32_t child_offset = offsets[offset + i];
     child_p->offset = child.offset + child_offset;
     child_p->length = offsets[offset + i + 1] - child_offset;
   }
 
-  NestedIterator<Nested> begin() const { return NestedIterator<Nested>(*this); }
-  NestedIterator<Nested> end() const { return NestedIterator<Nested>(*this, length); }
+  using Iterator = internal::NestedIterator<Nested>;
+  Iterator begin() const { return Iterator(*this); }
+  Iterator end() const { return Iterator(*this, length); }
+};
+
+namespace internal {
+
+template <typename T>
+void InitFromCoordView(T* value, struct GeoArrowCoordView* view) {
+  // TODO: remove
+  value->coord_view = *view;
+
+  value->offset = 0;
+  value->length = view->n_coords;
+  value->stride = view->coords_stride;
+  for (uint32_t i = 0; i < T::coord_size; i++) {
+    value->values[i] = view->values[i];
+  }
+}
+
+template <typename T>
+void InitFromArrayView(T* value, struct GeoArrowArrayView* view, int level) {
+  if constexpr (T::is_sequence) {
+    InitFromCoordView(value, view->coords);
+  } else {
+    value->offsets = view->offsets[level];
+    InitFromArrayView(&value->child, view, level + 1);
+  }
+
+  value->offset = view->offset[level];
+  value->length = view->length[level];
+};
+
+}  // namespace internal
+
+template <typename T>
+struct Array {
+  using value_type = T;
+  value_type value;
+  const uint8_t* validity;
+
+  void Init(struct GeoArrowArrayView* view) {
+    InitFromArrayView(this, view, 0);
+    validity = view->validity_bitmap;
+  }
+};
+
+template <typename Coord>
+struct PointArray : public Array<CoordSequence<Coord>> {
+  static constexpr enum GeoArrowGeometryType geometry_type = GEOARROW_GEOMETRY_TYPE_POINT;
+};
+
+template <typename Coord>
+struct LinestringArray : public Array<Nested<CoordSequence<Coord>>> {
+  static constexpr enum GeoArrowGeometryType geometry_type =
+      GEOARROW_GEOMETRY_TYPE_LINESTRING;
+};
+
+template <typename Coord>
+struct PolygonArray : public Array<Nested<Nested<CoordSequence<Coord>>>> {
+  static constexpr enum GeoArrowGeometryType geometry_type =
+      GEOARROW_GEOMETRY_TYPE_POLYGON;
+};
+
+template <typename Coord>
+struct MultipointArray : public Array<CoordSequence<Coord>> {
+  static constexpr enum GeoArrowGeometryType geometry_type =
+      GEOARROW_GEOMETRY_TYPE_MULTIPOINT;
+};
+
+template <typename Coord>
+struct MultiLinestringArray : public Array<Nested<Nested<CoordSequence<Coord>>>> {
+  static constexpr enum GeoArrowGeometryType geometry_type =
+      GEOARROW_GEOMETRY_TYPE_MULTILINESTRING;
+};
+
+template <typename Coord>
+struct MultiPolygonArray : public Array<Nested<Nested<CoordSequence<Coord>>>> {
+  static constexpr enum GeoArrowGeometryType geometry_type =
+      GEOARROW_GEOMETRY_TYPE_MULTIPOLYGON;
 };
 
 }  // namespace array
