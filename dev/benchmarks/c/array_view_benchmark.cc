7270da4cf840f0d401891690e97e29855f7c37de
Dewey Dunnington <dewey@wherobots.com>
2025-01-05 15:14:02 -0600

    update

--- dev/benchmarks/c/array_view_benchmark.cc
+++ dev/benchmarks/c/array_view_benchmark.cc
@@ -15,19 +15,14 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include <cmath>
 #include <cstdint>
-#include <iostream>
 
 #include <benchmark/benchmark.h>
 
 #include "geoarrow.h"
 
 #include "benchmark_lib.h"
-
-// The length of most arrays used in these benchmarks. Just big enough so
-// that the benchmark takes a non-trivial amount of time to run.
-static const int64_t kNumItemsPrettyBig = 100000000;
+#include "benchmark_util.hpp"
 
 enum CoordAccessStrategy { GENERIC, OPTIMIZED, LOOP_THEN_IF };
 
@@ -39,8 +34,8 @@ static void CoordViewLoop(benchmark::State& state) {
   struct GeoArrowArrayView view;
   GeoArrowArrayViewInitFromType(&view, type);
 
-  // Generate a circle with n_coords points
-  int64_t n_coords = kNumItemsPrettyBig;
+  // Memory for circle with n points
+  uint32_t n_coords = geoarrow::benchmark_util::kNumCoordsPrettyBig;
   int n_dims = view.coords.n_values;
   std::vector<double> coords(n_coords * n_dims);
   double angle_inc_radians = M_PI / 100;
@@ -51,26 +46,19 @@ static void CoordViewLoop(benchmark::State& state) {
     for (int i = 0; i < n_dims; i++) {
       view.coords.values[i] = coords.data() + (i * n_coords);
     }
-
-    for (int64_t i = 0; i < n_coords; i++) {
-      coords[i] = cos(angle) * radius;
-      coords[n_coords + i] = sin(angle) * radius;
-      angle += angle_inc_radians;
-    }
   } else {
     for (int i = 0; i < n_dims; i++) {
       view.coords.values[i] = coords.data() + i;
     }
-
-    for (int64_t i = 0; i < n_coords; i++) {
-      coords[n_dims * i] = cos(angle) * radius;
-      coords[n_dims * i + 1] = sin(angle) * radius;
-      angle += angle_inc_radians;
-    }
   }
 
+  std::array<double, 4> bounds{};
+
+  geoarrow::benchmark_util::PointsOnCircle(n_coords, view.coords.coords_stride,
+                                           const_cast<double*>(view.coords.values[0]),
+                                           const_cast<double*>(view.coords.values[1]));
+
   if (operation == BOUNDS) {
-    std::array<double, 4> bounds{};
     switch (strategy) {
       case GENERIC:
         for (auto _ : state) {
@@ -121,10 +109,10 @@ static void CoordViewLoop(benchmark::State& state) {
   }
 
   state.SetItemsProcessed(n_coords * state.iterations());
-  // Check the result
-  //   std::cout << bounds[0] << ", " << bounds[1] << ", " << bounds[2] << ", " <<
-  //   bounds[3]
-  //             << std::endl;
+  // Check the result (centroid should more or less be 0, 0; bounds should be more or less
+  // -484..483 in both dimensions)
+  // std::cout << bounds[0] << ", " << bounds[1] << ", " << bounds[2] << ", " << bounds[3]
+  //           << std::endl;
 }
 
 BENCHMARK(CoordViewLoop<BOUNDS, GEOARROW_TYPE_POINT, GENERIC>);
---
+++ dev/benchmarks/c/benchmark_util.hpp
@@ -0,0 +1,39 @@
+
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+
+namespace geoarrow {
+
+namespace benchmark_util {
+
+static const int64_t kNumCoordsPrettyBig = 10000000;
+
+static inline void PointsOnCircle(uint32_t n, uint32_t stride, double* out_x,
+                                  double* out_y, double dangle_radians = M_PI / 100.0,
+                                  double radius = 483.0) {
+  double angle = 0;
+
+  for (uint32_t i = 0; i < n; i++) {
+    *out_x = std::cos(angle) * radius;
+    *out_y = std::sin(angle) * radius;
+    angle += dangle_radians;
+    out_x += stride;
+    out_y += stride;
+  }
+}
+
+static inline void FillRandom(uint32_t n, uint32_t stride, double* out,
+                              uint32_t seed = 1234, double range_min = -1.0,
+                              double range_max = 1.0) {
+  std::srand(seed);
+  double range = range_max - range_max;
+  for (uint32_t i = 0; i < n; i++) {
+    double value01 = static_cast<double>(std::rand()) / static_cast<double>(RAND_MAX);
+    *out = range_min + value01 * range;
+    out += stride;
+  }
+}
+
+}  // namespace benchmark_util
+}  // namespace geoarrow
