6dc2afc432024a4d33ed63b4abaf7a921c2a5c6c
Dewey Dunnington <dewey@wherobots.com>
2025-01-08 11:22:42 -0600

    refine and document

--- src/geoarrow/geoarrow_ffi.h
+++ src/geoarrow/geoarrow_ffi.h
@@ -95,36 +95,150 @@ struct ArrowArrayStream {
 #endif  // ARROW_C_STREAM_INTERFACE
 #endif  // ARROW_FLAG_DICTIONARY_ORDERED
 
+/// \defgroup geoarrow-ffi GeoArrow ABI-Stable structures
+///
+/// This header provides the ABI-stable structures used for within-process
+/// interoperability among GeoArrow implementations.
+///
+/// @{
+
+/// \brief An errno-compatbile error code
 typedef int GeoArrowFFIErrorCode;
 
+/// \brief An allocator to use for large allocations
+///
+/// Most engines track memory usage very closely and provide a custom allocator
+/// to maximize performance. This structure allows the engine to specify how
+/// array buffers or other large intermediary buffers should be allocated.
 struct GeoArrowFFIAllocator {
+  /// \brief Allocate a new buffer or reallocate an old one
+  ///
+  /// New buffers are requested by ptr pointing to NULL and old_size equal to zero.
   GeoArrowFFIErrorCode (*reallocate)(struct GeoArrowFFIAllocator* self, uint8_t** ptr,
                                      int64_t old_size, int64_t new_size);
+
+  /// \brief Free a buffer allocated by reallocate
   void (*free)(struct GeoArrowFFIAllocator* self, uint8_t* ptr, int64_t old_size);
+
+  /// \brief Clone this allocator
+  ///
+  /// This will get
+  GeoArrowFFIErrorCode (*clone)(GeoArrowFFIAllocator* data, GeoArrowFFIAllocator* copy);
+
+  /// \brief Release any resources associated with this allocator and set the release
+  /// callback to NULL.
   void (*release)(struct GeoArrowFFIAllocator* self);
+
+  /// \brief Opaque, implementation-specific data
   void* private_data;
 };
 
+/// \brief State tracking a specific call to a function
+///
+/// Specifically, this is the result of a call to "bind" (i.e., input types
+/// and/or constant options are already known when this object is instantiated).
+/// The callbacks provided by this object perform the actual computation.
+/// Calls to these callbacks must be serialized/occur from the same thread; however,
+/// a clone callback is provided for cases where the engine cannot meet this guarantee.
 struct GeoArrowFFIFunctionState {
+  /// \brief Set the allocator that should be used for buffer allocations
+  ///
+  /// Implementations are not required to implement this callback but not doing
+  /// so may result in unexpected out-of-memory or degraded performance when used
+  /// with an engine that expects its use.
   GeoArrowFFIErrorCode (*set_allocator)(GeoArrowFFIFunctionState* data);
-  GeoArrowFFIErrorCode (*push)(GeoArrowFFIFunctionState* data, ArrowArray* array,
-                               int64_t n_arrays);
-  GeoArrowFFIErrorCode (*pull)(GeoArrowFFIFunctionState* data, struct ArrowArray* out);
+
+  /// \brief Populate an independent clone of this state that can be used concurrently
+  ///
+  /// Most engines do not organize their user-defined function interface in such a
+  /// way that batch calls are guaranteed to occur on the same thread. In this case,
+  /// an engine can clone a reference instance on each batch and use it to perform
+  /// the computation. Because of this, implementations of this callback should be
+  /// cheap.
+  GeoArrowFFIErrorCode (*clone)(GeoArrowFFIFunctionState* data,
+                                GeoArrowFFIFunctionState* copy);
+
+  /// \brief Push a batch of arguments into the compute function
+  ///
+  /// The arrays are interpreted according to the ArrowSchemas that were provided to the
+  /// bind callback that produced this state. The implementation should not perform
+  /// any computations in this step other than validating the structure of the input.
+  ///
+  /// Implementations for some functions may allow more than one batch to be pushed before
+  /// a call to pull; however, the canonical pattern for simple scalar functions
+  /// is a single pair of push/pull for each batch.
+  ///
+  /// The input arrays must have length of n_rows or 1. Constant values should be
+  /// represented by an array of length 1.
+  GeoArrowFFIErrorCode (*push)(GeoArrowFFIFunctionState* data, ArrowArray** arrays,
+                               int64_t n_arrays, int64_t n_rows);
+
+  /// \brief Compute and retrieve results of a calling the function
+  ///
+  /// Perform computation and pull n_rows of result into out (or a released out
+  /// to indicate there is no more output until another call to push).
+  GeoArrowFFIErrorCode (*pull)(GeoArrowFFIFunctionState* data, struct ArrowArray* out,
+                               int64_t n_rows);
+
+  /// \brief Retrieve a detailed error message from a previous erroring callback
+  ///
+  /// The result is valid until the next call to any callback, including release.
   const char* (*get_last_error)(struct GeoArrowFFIFunctionState* data);
+
+  /// \brief Release any resources associated with this state and set the release
+  /// callback to NULL.
   void (*release)(struct GeoArrowFFIFunctionState* self);
+
+  /// \brief Opaque, implementation-specific data
   void* private_data;
 };
 
+/// \brief An instance of a compute function
+///
+/// Typically there will be one instance of this object in some global registry
+/// wrapped in the engine's function registry as a user-defined function of some
+/// kind.
 struct GeoArrowFFIFunction {
+  /// \brief Bind a new instance of a GeoArrowFFIFunctionState and compute the output type
+  ///
+  /// This callback must be thread safe and reentrant (i.e., it can be called concurrently
+  /// from more than one thread at a time to produce independent function states).
   GeoArrowFFIErrorCode (*bind)(struct GeoArrowFFIFunction* self,
-                               const struct ArrowSchema** args, int64_t n_args,
-                               const char* metadata, struct ArrowSchema* out_type,
-                               struct GeoArrowFFIFunctionState* function_data_out);
+                               struct ArrowSchema** args, int64_t n_args,
+                               const char* options, struct ArrowSchema* out_schema,
+                               struct GeoArrowFFIFunctionState* out_function_data);
 
+  /// \brief Retrieve a detailed error message from a previous erroring callback
+  ///
+  /// The result is valid until the next call to any callback, including release.
   const char* (*get_last_error)(struct GeoArrowFFIFunction* self);
+
+  /// \brief Release any resources associated with this state and set the release
+  /// callback to NULL.
   void (*release)(struct GeoArrowFFIFunction* self);
+
+  /// \brief Opaque, implementation-specific data
   void* private_data;
 };
+
+struct GeoArrowFFICatalog {
+  GeoArrowFFIErrorCode get_function(const char* name, const char* options,
+                                    struct GeoArrowFFIFunction* out_function);
+
+  /// \brief Retrieve a detailed error message from a previous erroring callback
+  ///
+  /// The result is valid until the next call to any callback, including release.
+  const char* (*get_last_error)(struct GeoArrowFFIFunction* self);
+
+  /// \brief Release any resources associated with this catalog and set the release
+  /// callback to NULL.
+  void (*release)(struct GeoArrowFFICatalog* self);
+
+  /// \brief Opaque, implementation-specific data
+  void* private_data;
+};
+
+/// @}
 
 #ifdef __cplusplus
 }
