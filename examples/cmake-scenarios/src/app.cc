f90df3955e95dfcf9a8374af47295f45475cf965
Dewey Dunnington <dewey@wherobots.com>
2025-02-18 10:49:49 -0600

    fix app.cc to have a less confusing output string

--- examples/cmake-scenarios/src/app.cc
+++ examples/cmake-scenarios/src/app.cc
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[]) {
   // Use something from the geoarrow c runtime
-  std::printf("Schema format for int32 is '%s'\n", GeoArrowVersion());
+  std::printf("Version reported by the geoarrow-c runtime '%s'\n", GeoArrowVersion());
 
   return EXIT_SUCCESS;
 }
