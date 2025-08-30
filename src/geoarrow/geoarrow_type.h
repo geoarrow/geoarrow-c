27f56e0750093813333a78e1a866a3f039b30dfb
Dewey Dunnington <dewey@wherobots.com>
2025-08-29 20:35:24 -0500

    whoops

--- src/geoarrow/geoarrow_type.h
+++ src/geoarrow/geoarrow_type.h
@@ -222,7 +222,6 @@ struct ArrowArrayStream {
   _GEOARROW_MAKE_NAME(GEOARROW_NAMESPACE, GeoArrowWKBWriterInitVisitor)
 #define GeoArrowWKBWriterAppend \
   _GEOARROW_MAKE_NAME(GEOARROW_NAMESPACE, GeoArrowWKBWriterAppend)
-GeoArrowWKBWriterAppend
 #define GeoArrowWKBWriterFinish \
   _GEOARROW_MAKE_NAME(GEOARROW_NAMESPACE, GeoArrowWKBWriterFinish)
 #define GeoArrowWKBWriterReset \
